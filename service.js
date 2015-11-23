3031101fe83beda487525253c72b5fc8e58fe5a0
Jared Noble <jsnoble1@gmail.com>
2015-11-23 15:46:21 -0700

    first pass on multinode support

--- service.js
+++ service.js
@@ -2,12 +2,17 @@
 
 var worker = require('./lib/worker');
 var master = require('./lib/master');
+var slicer = require('./lib/distributed/slicer');
 var config_schema = require('./system_schema').config_schema;
 
 var foundation = require('terafoundation')({
     name: 'teraslice',
     worker: worker,
     master: master,
+    slicer: slicer,
+    descriptors: {slicer: true, worker: true},
     start_workers: false,
     config_schema: config_schema
 });
+
+//"2015-08-25T23:55:00",
