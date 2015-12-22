9493f774fc2fcf179b9e2b149950a0ab7873fae6
Jared Noble <jsnoble1@gmail.com>
2015-12-22 08:03:41 -0700

    added namespace capability

--- service.js
+++ service.js
@@ -3,17 +3,21 @@
 var worker = require('./lib/worker');
 var slicer = require('./lib/slicer');
 var master = require('./lib/master');
+var cluster_master = require('./lib/cluster/cluster_master');
 var config_schema = require('./system_schema').config_schema;
 var emitter = require('./lib/utils/events');
 
-var foundation = require('terafoundation')({
-    name: 'teraslice',
-    worker: worker,
-    master: master,
-    slicer: slicer,
-    shutdownMessaging: true,
-    descriptors: {slicer: true, worker: true},
-    start_workers: false,
-    config_schema: config_schema
-   // emitter: emitter
-});
+
+ var foundation = require('terafoundation')({
+ name: 'teraslice',
+ worker: worker,
+ master: master,
+ slicer: slicer,
+ shutdownMessaging: true,
+ cluster_master: cluster_master,
+ descriptors: {slicer: true, worker: true, cluster_master: true},
+ start_workers: false,
+ config_schema: config_schema
+ // emitter: emitter
+ });
+
