cfa979cdbd48c1b1238eb8576e3746a1333d83f0
Jared Noble <jsnoble1@gmail.com>
2016-02-02 13:50:23 -0700

    succesful rebase

--- service.js
+++ service.js
@@ -7,17 +7,26 @@ var cluster_master = require('./lib/cluster/cluster_master');
 var config_schema = require('./system_schema').config_schema;
 var emitter = require('./lib/utils/events');
 
+var foundation = require('terafoundation')({
+    name: 'teraslice',
+    worker: worker,
+    master: master,
+    slicer: slicer,
+    shutdownMessaging: true,
+    cluster_master: cluster_master,
+    descriptors: {slicer: true, worker: true, cluster_master: true},
+    start_workers: false,
+    config_schema: config_schema
+    // emitter: emitter
+});
 
- var foundation = require('terafoundation')({
- name: 'teraslice',
- worker: worker,
- master: master,
- slicer: slicer,
- shutdownMessaging: true,
- cluster_master: cluster_master,
- descriptors: {slicer: true, worker: true, cluster_master: true},
- start_workers: false,
- config_schema: config_schema
- // emitter: emitter
- });
+
+/*
+ "cluster": {
+ "port": 5678,
+ "worker_port": 6789,
+ "host": "localhost:",
+ "master": true
+ },
+ */
 
