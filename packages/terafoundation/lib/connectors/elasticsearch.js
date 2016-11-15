3c5bb764be69349d69e1314ee29ecccaa5327072
Austin Godber <godber@uberhip.com>
2016-11-15 08:42:47 -0700

    Upgrading ES lib and explicitly using bluebird to support ES 5 (#69)
    The following changes were needed to support ES 5.0:
    
    * Upgrade the ES js client to the current version `^12.0.0`
    * Version 10 of the ES js client switched to native promises
      which required that we explicitly defer to Bluebird to
      maintain compatibility with our existing code base.
    
    refs #68

--- packages/terafoundation/lib/connectors/elasticsearch.js
+++ packages/terafoundation/lib/connectors/elasticsearch.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var Promise = require('bluebird');
 var Agent = require('./elastic_helpers');
 
 function logWrapper(logger) {
@@ -29,6 +30,9 @@ function create(customConfig, logger) {
     logger.info("Using elasticsearch hosts: " + customConfig.host);
 
     customConfig.connectionClass = Agent;
+    customConfig.defer = function () {
+        return Promise.defer();
+    }
     var client = new elasticsearch.Client(customConfig);
 
     return {
