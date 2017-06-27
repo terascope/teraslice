d2dc38c40d7338d5f4fc2a983b305e9f02f2e542
Austin Godber <godber@uberhip.com>
2017-06-27 08:59:26 -0700

    fleshed out a bit

---
+++ packages/teraslice_op_test_harness/README.md
@@ -0,0 +1,92 @@
+# Teraslice Operation Test Harness
+
+This project provides a number of mock objects, data sources and functions to
+help you implement tests of your Teraslice operations.  But first, to start off,
+require your processor and then pass in your processor name as an argument to
+the harness module as shown below.  The code examples below assume you are using
+the harness in a `spec/` or `test/` subdirectory and your processor is
+implemented in `../index`.
+
+```javascript
+var processor = require('../index');
+var harness = require('teraslice_op_test_harness')('dupedoc');
+```
+
+Now, you can access functionality using the `harness` object.
+
+## Mock Objects
+
+The testing harness provides a number of mocked resources that are needing when
+testing a Teraslice processor or operation.  The first few resources you will
+need are a `context`, an `op`, and a `jobConfig`.  These are needed when you
+create your processor and you can use the standard mock resources like this:
+
+```javascript
+var processor = require('../index');
+var harness = require('teraslice_op_test_harness')('dupedoc');
+var myProcessor = processor.newProcessor(
+    harness.context,
+    harness.op,
+    harness.jobConfig);
+```
+
+If you want to override the default operations on your processor, you can do so
+by setting new properties on `op`.  Note that depending on your use case you may
+want to clone `harness.op` to avoid impacting other tests as shown below.
+
+```javascript
+var processor = require('../index');
+var harness = require('teraslice_op_test_harness')('dupedoc');
+var op = _.cloneDeep(harness.op);
+// setting processors percentage argument to 100
+op.percentage = 100;
+
+var myProcessor = processor.newProcessor(
+    harness.context,
+    op,
+    harness.jobConfig);
+```
+
+The other resource you're likely to need is a mock logging object.  When calling
+your processor, the first argument is the data and the second argument is the
+`sliceLogger`, which must behave like a bunyan compatible logger.  The
+`fakeLogger.logger` object serves that purpose as shown below.
+
+```javascript
+var _ = require('lodash');
+var processor = require('../index');
+var harness = require('teraslice_op_test_harness')('dupedoc');
+var sliceLogger = harness.fakeLogger.logger;
+var myProcessor = processor.newProcessor(
+    harness.context,
+    harness.op,
+    harness.jobConfig);
+
+myProcessor([], sliceLogger)
+```
+
+## Data Sources
+
+One of the main benefits of the testing harness is that it provides a consistent
+set of testing data sources.  All of these objects are properties of the harness
+in `data` property.  The following data sources are currently available:
+
+* `data.simple` - A very small and simple array of JSON objects.
+* `data.arrayLike` - An array of 10 "log like" JSON documents.
+* `data.esLike` - The same 10 documents as `data.arrayLike`, but structured like
+  an Elasticsearch query response.
+
+These data sources can be used as the first argument to your processor in tests
+as shown below.
+
+```javascript
+var processor = require('../index');
+var harness = require('teraslice_op_test_harness')('dupedoc');
+var sliceLogger = harness.fakeLogger.logger;
+var myProcessor = processor.newProcessor(
+    harness.context,
+    harness.op,
+    harness.jobConfig);
+
+myProcessor(harness.data.simple, sliceLogger)
+```
--- packages/teraslice_op_test_harness/index.js
+++ packages/teraslice_op_test_harness/index.js
@@ -1,42 +1,71 @@
 var sampleDataArrayLike = require('./data/sampleDataArrayLike.json');
 var sampleDataEsLike = require('./data/sampleDataEsLike.json');
 
+var fakeLogger = {
+    logger: {
+        fatal: function() {},
+        error: function() {},
+        warn: function() {},
+        info: function() {},
+        debug: function() {},
+        trace: function() {},
+    }
+};
+
+var simpleData = [
+    {'name': 'Skippy', 'age': 20},
+    {'name': 'Flippy', 'age': 21},
+    {'name': 'Hippy', 'age': 22},
+    {'name': 'Dippy', 'age': 23},
+];
+
+
 /**
  * Teraslice Processor Test Framework
  * @module teraslice_processor_test_framework
  */
-module.exports = {
-    /** Fake logger object with empty method definitions.  Suitable for use as
-     *  the general teraslice logger or as the sliceLogger.  Implements the
-     *  following log levels:
-     *    - `fatal`
-     *    - error
-     *    - warn
-     *    - info
-     *    - debug
-     *    - trace
-     *  Which are derived from bunyan's default levels:
-     *    https://github.com/trentm/node-bunyan#levels
-     */
-    fakeLogger: {
-        logger: {
-            fatal: function() {},
-            error: function() {},
-            warn: function() {},
-            info: function() {},
-            debug: function() {},
-            trace: function() {},
-        },
-    },
-    /** Standard test data objects: arrayLike and esLike */
-    data: {
-        /** Sample data in the form of an array of JSON documents , like would
-        *   come from the elasticsearch_data_generator
-        */
-        arrayLike: sampleDataArrayLike,
-        /** Sample data in the form of an array of Elasticsearch query response.
-         *   documents
+module.exports = (op) => {
+    return {
+        /* Setup mock contexts for processor, each processor takes:
+         *   context - global teraslice/terafoundation context object
+         *   op - configuration of the specific operation being executed (the processor)
+         *   jobConfig - details on this jobs configuration
+         *   sliceLogger - a logger instance for each slice
          */
-        esLike: sampleDataEsLike
-    }
+        context: {},
+        op: {'_op': op},
+        jobConfig: {fakeLogger},
+
+        /** Fake logger object with empty method definitions.  Suitable for use as
+         *  the general teraslice logger or as the sliceLogger.  Implements the
+         *  following log levels:
+         *    - fatal
+         *    - error
+         *    - warn
+         *    - info
+         *    - debug
+         *    - trace
+         *  Which are derived from bunyan's default levels:
+         *    https://github.com/trentm/node-bunyan#levels
+         */
+        fakeLogger: fakeLogger,
+
+        /** Standard test data objects: arrayLike and esLike */
+        data: {
+            /**
+             * A very simple and small array of JSON objects
+             */
+            simple: simpleData,
+            /**
+             * Sample data in the form of an array of JSON documents , like would
+             *   come from the elasticsearch_data_generator
+             */
+            arrayLike: sampleDataArrayLike,
+            /**
+             * Sample data in the form of an array of Elasticsearch query response.
+             *   documents
+             */
+            esLike: sampleDataEsLike
+        }
+    };
 };
