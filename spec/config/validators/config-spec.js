669ada2bb4dbe2b703ca12faa04e971bd46ffd6d
Austin Godber <godber@uberhip.com>
2017-07-12 13:42:33 -0700

    Extract validateOperation from validators.job
    To support development of the Teraslice Test harness, specifically
    https://github.com/terascope/teraslice_op_test_harness/issues/2
    
    I have extracted validateOperation.  It is renamed to
    validateConfig since it handles both jobConfigs and opConfigs.
    
    I have also removed the third argument, which was a boolean
    that indicated whether the config passes was an operation or not.
    I replaced it with a test for the presence of ._op which should
    be present on all operations.

---
+++ spec/config/validators/config-spec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var configValidator = require('../../../lib/config/validators/config')();
+
+describe('system_schema', function() {
+    it('schema has defaults', function() {
+        var context = {
+            sysconfig: {
+                teraslice: {
+                    ops_directory: ''
+                }
+            }
+        };
+
+        var jobSchema = require('../../../lib/config/schemas/job').jobSchema(context);
+        var jobSpec = {
+            'operations': [{
+                    '_op': 'noop'
+                },
+                {
+                    '_op': 'noop'
+                }
+            ]
+        };
+        var validJob = {
+            name: 'Custom Job',
+            lifecycle: 'once',
+            analytics: true,
+            max_retries: 3,
+            slicers: 1,
+            workers: 4,
+            operations: [
+                {_op: 'noop'},
+                {_op: 'noop'}
+            ],
+            assets: null,
+            moderator: null
+        };
+
+        var jobConfig = configValidator.validateConfig(jobSchema, jobSpec);
+        expect(jobConfig).toEqual(validJob);
+    });
+});
