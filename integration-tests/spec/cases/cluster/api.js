52cb6acc270e542b4cda7b23989d75afad0c18ba
Jared Noble <jsnoble1@gmail.com>
2017-09-13 09:03:06 -0700

    fixed how fail worked

--- integration-tests/spec/cases/cluster/api.js
+++ integration-tests/spec/cases/cluster/api.js
@@ -35,7 +35,7 @@ module.exports = function() {
                         })
 
                 })
-                .catch(fail())
+                .catch(fail)
                 .finally(done);
         });
     });
