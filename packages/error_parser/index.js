1ff95c2649bd63262fae158cd034410312ec78e9
Jared Noble <jsnoble1@gmail.com>
2017-11-09 09:14:12 -0700

    making error parser module

---
+++ packages/error_parser/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const _ = require('lodash');
+
+module.export = function parseError(err) {
+    if (err.toJSON) {
+        if (_.get(err, 'body.error.type') === 'index_not_found_exception') {
+            return `error: index_not_found_exception, could not find index: ${err.body.error.index}`
+        }
+        if (_.get(err, 'body.error.type') === 'search_phase_execution_exception') {
+            const cause = _.get(err, 'body.error.root_cause[0]', {});
+            return `error: ${cause.type} ${cause.reason} on index: ${cause.index}`
+        }
+        const esError = err.toJSON();
+        if (esError.msg) {
+            return esError.msg;
+        }
+        else {
+            return "Unknown ES Error Format " + JSON.stringify(err);
+        }
+    }
+    if (err.stack) {
+        return err.stack
+    }
+    return err.response ? err.response : err;
+};
