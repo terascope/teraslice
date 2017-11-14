fcb0843dea60fa4891b454be8b229ec4401cdcae
Jared Noble <jsnoble1@gmail.com>
2017-11-14 14:32:28 -0700

    added tests

---
+++ packages/error_parser/spec/module-spec.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const errorParser = require('../index.js');
+
+describe('error_parser', () => {
+
+    it('returns a function', () =>{
+        expect(typeof errorParser).toEqual('function')
+    });
+
+    it('can parse regular errors', () => {
+        const message = 'i am an error';
+        const error = new Error(message);
+        const parsedError= errorParser(error);
+
+        expect(parsedError).toEqual(error.stack)
+    });
+
+    it('can handle other error responses or return error as it', () => {
+        const responseError = { response: 'i am an error response' };
+        const other = 'i return as is';
+
+        expect(errorParser(responseError)).toEqual(responseError.response);
+        expect(errorParser(other)).toEqual(other);
+    });
+
+    it('can handle elasticsearch errors', () => {
+       const msg = 'i am a elasticsearch error';
+       const errorData = {
+            toJSON(){ return { msg }}
+        };
+
+       expect(errorParser(errorData)).toEqual(msg)
+    });
+
+    it('can return better error messages for index not found errors', () => {
+        const msg = 'i am a elasticsearch error';
+        const errorData = {
+            toJSON(){ return { msg }},
+            body: {
+                error: {
+                    type: 'index_not_found_exception',
+                    index: 'someIndex'
+                }
+            }
+        };
+        const expectedErrorMsg = `error: index_not_found_exception, could not find index: ${errorData.body.error.index}`
+        expect(errorParser(errorData)).toEqual(expectedErrorMsg)
+    });
+
+    it('can return better error messages for search errors', () => {
+        const msg = 'i am a elasticsearch error';
+        const cause = {
+            type: 'errorType',
+            index: 'someIndex',
+            reason: 'justBecause'
+        };
+        const errorData = {
+            toJSON(){ return { msg }},
+            body: {
+                error: {
+                    type: 'search_phase_execution_exception',
+                    root_cause: [cause]
+                }
+            }
+        };
+        const expectedErrorMsg = `error: ${cause.type} ${cause.reason} on index: ${cause.index}`;
+        expect(errorParser(errorData)).toEqual(expectedErrorMsg)
+    })
+
+});
