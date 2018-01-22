cd2c9abbd02ef5a9b8f314a882a7b1a43418c6c2
Jared Noble <jsnoble1@gmail.com>
2018-01-22 14:18:48 -0700

    fixed asset integration issues and added tests to verify job post/update changes

--- integration-tests/spec/misc.js
+++ integration-tests/spec/misc.js
@@ -1,61 +1,60 @@
 'use strict';
 
-var _ = require('lodash');
-var Promise = require('bluebird');
-var TerasliceClient = require('teraslice-client-js');
-var ElasticsearchClient = require('elasticsearch').Client;
+const _ = require('lodash');
+const Promise = require('bluebird');
+const TerasliceClient = require('teraslice-client-js');
+const ElasticsearchClient = require('elasticsearch').Client;
 
-module.exports = function() {
-
-    var DOCKER_IP = process.env.ip ? process.env.ip : 'localhost';
-    var compose = require('docker-compose-js')('docker-compose.yml');
+module.exports = function () {
+    const DOCKER_IP = process.env.ip ? process.env.ip : 'localhost';
+    const compose = require('docker-compose-js')('docker-compose.yml');
 
     function newJob(name) {
-        return _.cloneDeep(require(`./fixtures/jobs/${name}.json`))
+        return _.cloneDeep(require(`./fixtures/jobs/${name}.json`));
     }
 
     function teraslice() {
         return TerasliceClient({
             host: `http://${DOCKER_IP}:45678`
-        })
+        });
     }
 
     function es() {
         return new ElasticsearchClient({
             host: `http://${DOCKER_IP}:49200`,
             log: '' // This suppresses error logging from the ES library.
-        })
+        });
     }
 
     function indexStats(indexName) {
-        return new Promise(function(resolve, reject) {
-            es().indices.refresh({index: indexName})
-                .then(function(result) {
-                    es().indices.stats({index: indexName})
-                        .then(function(stats) {
-                            resolve(stats._all.total.docs)
+        return new Promise(((resolve, reject) => {
+            es().indices.refresh({ index: indexName })
+                .then(() => {
+                    es().indices.stats({ index: indexName })
+                        .then((stats) => {
+                            resolve(stats._all.total.docs);
                         })
-                        .catch(function(err) {
-                            reject(err)
-                        })
+                        .catch((err) => {
+                            reject(err);
+                        });
                 })
-                .catch(function(err) {
-                    reject(err)
-                })
-        })
+                .catch((err) => {
+                    reject(err);
+                });
+        }));
     }
 
     // Adds teraslice-workers to the environment
     function scale(count) {
-        return compose.scale('teraslice-worker=' + count)
+        return compose.scale(`teraslice-worker=${count}`);
     }
 
     return {
-        newJob: newJob,
+        newJob,
         teraslice: _.memoize(teraslice),
         es: _.memoize(es),
-        indexStats: indexStats,
-        compose: compose,
-        scale: scale
-    }
+        indexStats,
+        compose,
+        scale
+    };
 };
--- integration-tests/spec/setup.js
+++ integration-tests/spec/setup.js
@@ -1,150 +1,146 @@
 'use strict';
 
-var _ = require('lodash');
-var Promise = require('bluebird');
-var misc = require('./misc')();
+const _ = require('lodash');
+const Promise = require('bluebird');
+const misc = require('./misc')();
+const fs = require('fs');
 
 // We need long timeouts for some of these jobs
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
 
 if (process.stdout.isTTY) {
-    var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+    const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
     jasmine.getEnv().clearReporters();
     jasmine.getEnv().addReporter(new SpecReporter({
         spec: {
             displayStacktrace: true,
             displayDuration: true
         }
-    }))
+    }));
 }
 
-describe('teraslice', function() {
-
+describe('teraslice', () => {
     function dockerUp() {
         console.log(' - Bringing Docker environment up');
-        return misc.compose.up({build: ''})
+        return misc.compose.up({ build: '' });
     }
 
     function waitForES() {
         process.stdout.write(' - Waiting for Elasticsearch...');
-        return new Promise(function(resolve, reject) {
-            var attempts = 0;
+        return new Promise(((resolve, reject) => {
+            let attempts = 0;
 
             function wait() {
-                attempts++;
+                attempts += 1;
                 misc.es().cat.indices({
                     requestTimeout: 1000
                 })
-                    .then(function() {
+                    .then(() => {
                         console.log(' Ready');
-                        resolve()
+                        resolve();
                     })
-                    .catch(function(err) {
+                    .catch(() => {
                         if (attempts > 50) {
                             console.log(' Giving up');
-                            reject('timed out')
-                        }
-                        else {
+                            reject('timed out');
+                        } else {
                             process.stdout.write('.');
-                            setTimeout(wait, 1000)
+                            setTimeout(wait, 1000);
                         }
-                    })
+                    });
             }
 
-            wait()
-        })
+            wait();
+        }));
     }
 
     function waitForTeraslice() {
         process.stdout.write(' - Waiting for Teraslice...');
-        return new Promise(function(resolve, reject) {
-            var attempts = 0;
-            var check = misc.teraslice().cluster.state;
+        return new Promise(((resolve, reject) => {
+            let attempts = 0;
 
             function wait() {
-                attempts++;
-                check()
-                    .then(function() {
+                attempts += 1;
+                misc.teraslice().cluster.txt('assets')
+                    .then(() => {
                         console.log(' Ready');
-                        resolve()
+                        resolve();
                     })
-                    .catch(function(err) {
+                    .catch(() => {
                         if (attempts > 50) {
                             console.log(' Giving up');
-                            reject('timed out')
-                        }
-                        else {
+                            reject('timed out');
+                        } else {
                             process.stdout.write('.');
-                            setTimeout(wait, 1000)
+                            setTimeout(wait, 1000);
                         }
-                    })
+                    });
             }
 
-            wait()
-        })
+            wait();
+        }));
     }
 
     function cleanup() {
         console.log(' - Cleaning up teraslice state & prior test results');
-        var deletions = [
-            misc.es().indices.delete({index: 'teracluster__*', ignore: [404]}),
-            misc.es().indices.delete({index: 'test-*', ignore: [404]})
+        const deletions = [
+            misc.es().indices.delete({ index: 'teracluster__*', ignore: [404] }),
+            misc.es().indices.delete({ index: 'test-*', ignore: [404] })
         ];
-        return Promise.all(deletions)
+        return Promise.all(deletions);
     }
 
     function generateTestData() {
         console.log(' - Making sure example data generated');
 
         function generate(count, hex) {
-            var indexName = 'example-logs-' + count;
+            let indexName = `example-logs-${count}`;
             if (hex) {
-                indexName += '-hex'
+                indexName += '-hex';
             }
-            var job_spec = {
-                'name': 'Generate: ' + indexName,
-                'lifecycle': 'once',
-                'workers': 1,
-                'operations': [
+            const jobSpec = {
+                name: `Generate: ${indexName}`,
+                lifecycle: 'once',
+                workers: 1,
+                operations: [
                     {
-                        '_op': 'elasticsearch_data_generator',
-                        'size': count
+                        _op: 'elasticsearch_data_generator',
+                        size: count
                     },
                     {
-                        '_op': 'elasticsearch_index_selector',
-                        'index': indexName,
-                        'type': 'events'
+                        _op: 'elasticsearch_index_selector',
+                        index: indexName,
+                        type: 'events'
                     },
                     {
-                        '_op': 'elasticsearch_bulk',
-                        'size': 5000
+                        _op: 'elasticsearch_bulk',
+                        size: 5000
                     }
                 ]
             };
-            return new Promise(function(resolve, reject) {
-                misc.es().indices.stats({index: indexName}, function(err, stats) {
+            return new Promise(((resolve) => {
+                misc.es().indices.stats({ index: indexName }, (err, stats) => {
                     if (_.get(stats, '_all.total.docs.count') === count) {
                         resolve();
-                        return
+                        return;
                     }
                     console.warn('    - defining index:', indexName, ', reason:', _.get(stats, '_all.total.docs.count', 'index_not_found'));
-                    misc.es().indices.delete({index: indexName}, function() {
+                    misc.es().indices.delete({ index: indexName }, () => {
                         if (!hex) {
-                            resolve(misc.teraslice().jobs.submit(job_spec))
+                            resolve(misc.teraslice().jobs.submit(jobSpec));
+                        } else {
+                            jobSpec.operations[0].size = count / hex.length;
+                            jobSpec.operations[0].set_id = 'hexadecimal';
+                            jobSpec.operations[1].id_field = 'id';
+                            resolve(_.map(hex, (letter) => {
+                                jobSpec.name = `Generate: ${indexName}[${letter}]`;
+                                jobSpec.operations[0].id_start_key = letter;
+                                return misc.teraslice().jobs.submit(jobSpec);
+                            }));
                         }
-                        else {
-                            job_spec.operations[0].size = count / hex.length;
-                            job_spec.operations[0].set_id = 'hexadecimal';
-                            job_spec.operations[1].id_field = 'id';
-                            resolve(_.map(hex, function(letter) {
-                                job_spec.name = `Generate: ${indexName}[${letter}]`;
-                                job_spec.operations[0].id_start_key = letter;
-                                return misc.teraslice().jobs.submit(job_spec)
-                            }))
-                        }
-                    })
-                })
-            })
+                    });
+                });
+            }));
         }
 
         return Promise.all([
@@ -156,28 +152,29 @@ describe('teraslice', function() {
             .then(_.filter)
             .then(_.flatten)
             .map(job => job.waitForStatus('completed'))
-            .catch(function(err) {
+            .catch((err) => {
                 console.error('Data generation failed: ', err);
-                process.exit(1)
-            })
+                process.exit(1);
+            });
     }
 
-    var before = [dockerUp, waitForES, waitForTeraslice, cleanup, generateTestData]
+    const before = [dockerUp, waitForES, waitForTeraslice, cleanup, generateTestData];
 
-    beforeAll(function(done) {
+    beforeAll((done) => {
         Promise.resolve(before)
             .mapSeries(f => f())
-            .then(function() {
+            .then(() => {
                 console.log('Global setup complete. Starting tests...');
-                done()
+                done();
             })
-            .catch(function(err) {
+            .catch((err) => {
                 console.error('Setup failed: ', err, ' - `docker-compose logs` may provide clues');
-                process.exit(2)
-            })
+                process.exit(2);
+            });
     });
 
     require('./cases/cluster/api')();
+    require('./cases/assets/simple')();
     require('./cases/cluster/job-state')();
     require('./cases/data/id-reader')();
     require('./cases/data/elasticsearch-bulk')();
@@ -185,5 +182,4 @@ describe('teraslice', function() {
     require('./cases/cluster/worker-allocation')();
     require('./cases/cluster/state')();
     require('./cases/validation/job')();
-    require('./cases/assets/simple')();
 });
