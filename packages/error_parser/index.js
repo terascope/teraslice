82f1853810e60cb6589a3d9aabee3bf70be4ed0a
Kimbro Staken <kstaken@kstaken.com>
2018-06-11 13:38:43 -0700

    Merge pull request #3 from terascope/automate-and-be-consistent
    v1.0.0 automate and be consistent

--- packages/error_parser/index.js
+++ packages/error_parser/index.js
@@ -5,26 +5,25 @@ const _ = require('lodash');
 module.exports = function parseError(err) {
     if (err.toJSON) {
         if (_.get(err, 'body.error.type') === 'index_not_found_exception') {
-            return `error: index_not_found_exception, could not find index: ${err.body.error.index}`
+            return `error: index_not_found_exception, could not find index: ${err.body.error.index}`;
         }
         if (_.get(err, 'body.error.type') === 'search_phase_execution_exception') {
             const cause = _.get(err, 'body.error.root_cause[0]', {});
-            return `error: ${cause.type} ${cause.reason} on index: ${cause.index}`
+            return `error: ${cause.type} ${cause.reason} on index: ${cause.index}`;
         }
         const esError = err.toJSON();
         if (esError.msg) {
             return esError.msg;
         }
-        else {
-            return "Unknown ES Error Format " + JSON.stringify(err);
-        }
+
+        return `Unknown ES Error Format ${JSON.stringify(err)}`;
     }
     if (err.stack) {
-        return err.stack
+        return err.stack;
     }
-    if (err.response){
+    if (err.response) {
         return err.response;
     }
     // in a hdfs error it can return the entire data set so we make sure to only take up to 5k chars
-    return err.length >= 5000 ? `${err.slice(0,5000)} ...` : err;
+    return err.length >= 5000 ? `${err.slice(0, 5000)} ...` : err;
 };
