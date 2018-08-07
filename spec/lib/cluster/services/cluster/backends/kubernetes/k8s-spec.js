f8bb5f48c29fcb683e7320ec251a59338f01b7da
peterdemartini <thepeterdemartini@gmail.com>
2018-08-07 08:26:23 -0700

    use a better templating engine and base64 encode the execution

--- spec/lib/cluster/services/cluster/backends/kubernetes/k8s-spec.js
+++ spec/lib/cluster/services/cluster/backends/kubernetes/k8s-spec.js
@@ -28,8 +28,7 @@ const swaggerFile = path.join(__dirname, 'files', 'swagger.json');
 const _url = 'http://mock.kube.api';
 // const _url = 'https://192.168.99.100:8443';
 
-// FIXME: Remember, I break unit tests if I leave this in!!
-fdescribe('k8s', () => {
+describe('k8s', () => {
     let k8s;
 
     beforeEach(async () => {
