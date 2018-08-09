78a390dd2e6f2e41af4b5f97c4cb77b69b6e1fff
peterdemartini <thepeterdemartini@gmail.com>
2018-08-09 10:02:24 -0700

    fixes to teraslice-op-test-harness

--- packages/teraslice-op-test-harness/.dir-locals.el
+++
@@ -1,8 +0,0 @@
-((nil . ((eval . (progn
-                   (add-to-list 'exec-path (concat (locate-dominating-file default-directory ".dir-locals.el") "node_modules/.bin/"))
-                   (custom-set-variables
-                    '(c-basic-offset 4)
-                    '(js-indent-level 4)
-                    '(js2-basic-offset 4)
-                    )
-                   )))))
