9cf9d30b516330b11fb133385c6c9e42edf9c0bf
macgyver603 <zachary.burnham@gmail.com>
2018-08-22 13:04:41 -0700

    Merge pull request #3 from terascope/add-test-coverage
    Add test coverage

--- packages/chunked-file-reader/index.js
+++ packages/chunked-file-reader/index.js
@@ -2,21 +2,22 @@
 
 const chunkFormatter = require('./lib/formatters');
 
+
+function _averageRecordSize(array) {
+    return Math.floor(array.reduce((accum, str) => accum + str.length, 0) / array.length);
+}
+
+
 // This function will grab the chunk of data specified by the slice plus an extra margin if the
 // slice is not at the end of the file. It needs to read the file twice, first grabbing the data
 // specified in the slice and then the margin, which gets appended to the data
 function getChunk(readerClient, slice, opConfig, logger) {
-    function averageRecordSize(array) {
-        return Math.floor(array.reduce((accum, str) => accum + str.length, 0) / array.length);
-    }
-
     function getMargin(marginOpts) {
         // This grabs the start of the margin up to the first delimiter, which should be the
         // remainder of a truncated record
         return readerClient(marginOpts.offset, marginOpts.length)
             .then(data => data.split(opConfig.delimiter)[0]);
     }
-
 
     let needMargin = false;
     if (slice.length) {
@@ -35,23 +36,23 @@ function getChunk(readerClient, slice, opConfig, logger) {
                 needMargin = false;
             }
             if (needMargin) {
-                const avgSize = averageRecordSize(data.split(opConfig.delimiter));
+                const avgSize = _averageRecordSize(data.split(opConfig.delimiter));
                 const marginOptions = {};
                 // Safety margin of two average-sized records
                 marginOptions.length = avgSize * 2;
                 marginOptions.offset = slice.offset + slice.length;
                 return getMargin(marginOptions)
-                    .then(margin => cleanData(`${data}${margin}`, slice, opConfig));
+                    .then(margin => _cleanData(`${data}${margin}`, slice, opConfig));
             }
             // logger.info(data);
-            return cleanData(data, slice, opConfig);
+            return _cleanData(data, slice, opConfig);
         })
         .then(data => chunkFormatter[opConfig.format](data, logger));
 }
 
 // This function takes the raw data and breaks it into records, getting rid of anything preceding
 // the first complete record if the data does not start with a complete record
-function cleanData(rawData, slice, opConfig) {
+function _cleanData(rawData, slice, opConfig) {
     /* Since slices with a non-zero chunk offset grab the character immediately preceding the main
      * chunk, if one of those chunks has a delimiter as the first or second character, it means the
      * chunk starts with a complete record. In this case as well as when the chunk begins with a
@@ -76,5 +77,6 @@ function cleanData(rawData, slice, opConfig) {
 }
 
 module.exports = {
-    getChunk
+    getChunk,
+    _averageRecordSize
 };
