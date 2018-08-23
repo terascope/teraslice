66f803960bbc7c8c2e832839bf06d7168c9f263a
Austin Godber <godber@uberhip.com>
2018-08-23 15:06:51 -0700

    Add support for setting k8s affinity by job
    This should resolve #794

--- packages/teraslice/lib/cluster/services/cluster/backends/kubernetes/k8sDeployment.js
+++ packages/teraslice/lib/cluster/services/cluster/backends/kubernetes/k8sDeployment.js
@@ -5,8 +5,41 @@ const { makeTemplate } = require('./utils');
 
 const workerDeploymentTemplate = makeTemplate('deployments', 'worker');
 
+/**
+ * Generate the kubernetes worker deployment for a teraslice job worker
+ * from the execution (job instance definition) and the config context
+ * @param  {Object} execution Teraslice Execution object
+ * @param  {Object} config    Configuration object with calling context info
+ * @return {Object}           Worker Deployment Object
+ */
 function gen(execution, config) {
     const workerDeployment = workerDeploymentTemplate(config);
+
+    // Apply job `node_labels` setting as k8s nodeAffinity
+    // We assume that multiple node_labels require both to match ...
+    // NOTE: If you specify multiple `matchExpressions` associated with
+    // `nodeSelectorTerms`, then the pod can be scheduled onto a node
+    // only if *all* `matchExpressions` can be satisfied.
+    if (_.has(execution, 'node_labels')) {
+        workerDeployment.spec.template.spec.affinity = {
+            nodeAffinity: {
+                requiredDuringSchedulingIgnoredDuringExecution: {
+                    nodeSelectorTerms: [{ matchExpressions: [] }]
+                }
+            }
+        };
+
+        _.forEach(execution.node_labels, (label) => {
+            workerDeployment.spec.template.spec.affinity.nodeAffinity
+                .requiredDuringSchedulingIgnoredDuringExecution
+                .nodeSelectorTerms[0].matchExpressions.push({
+                    key: label.key,
+                    operator: 'In',
+                    values: [label.value]
+                });
+        });
+    }
+
 
     return workerDeployment;
 }
---
+++ packages/teraslice/test/lib/cluster/services/cluster/backends/kubernetes/k8sDeployment-spec.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const _ = require('lodash');
+const yaml = require('js-yaml');
+const _execution = require('./files/execution.json');
+const k8sDeployment = require('../../../../../../../lib/cluster/services/cluster/backends/kubernetes/k8sDeployment');
+const { base64EncodeObject } = require('../../../../../../../lib/cluster/services/cluster/backends/kubernetes/utils');
+
+const _name = `teraslice-worker-${_execution.ex_id}`.substring(0, 63);
+const _config = {
+    name: _name,
+    exId: _execution.ex_id,
+    jobId: _execution.job_id,
+    dockerImage: 'teraslice-k8sdev:1',
+    execution: base64EncodeObject(_execution),
+    nodeType: 'worker',
+    namespace: 'ts-dev1',
+    shutdownTimeout: 30000,
+    replicas: 1,
+    configMapName: 'teraslice-worker',
+    imagePullSecret: 'teraslice-secret',
+};
+
+
+describe('k8sDeployment', () => {
+    let ex;
+    let config;
+
+    beforeEach(() => {
+        ex = _.cloneDeep(_execution);
+        config = _.cloneDeep(_config);
+    });
+
+    it('should render a simple deployment with the expected exId', () => {
+        const deployment = k8sDeployment.gen(ex, config);
+        expect(deployment.metadata.labels.exId).toEqual('e76a0278-d9bc-4d78-bf14-431bcd97528c');
+    });
+
+    describe('with a single value in node_labels', () => {
+        it('should render a deployment with a required affinity', () => {
+            ex.node_labels = [{ key: 'zone', value: 'west' }];
+            const deployment = k8sDeployment.gen(ex, config);
+
+            expect(deployment.metadata.labels.exId).toEqual('e76a0278-d9bc-4d78-bf14-431bcd97528c');
+            expect(deployment.spec.template.spec.affinity).toEqual(yaml.load(`
+                nodeAffinity:
+                  requiredDuringSchedulingIgnoredDuringExecution:
+                    nodeSelectorTerms:
+                    - matchExpressions:
+                      - key: zone
+                        operator: In
+                        values:
+                        - west`));
+        });
+    });
+
+    describe('with multiple single node_labels', () => {
+        it('should render a deployment with a required affinity', () => {
+            ex.node_labels = [
+                { key: 'zone', value: 'west' },
+                { key: 'region', value: '42' }
+            ];
+            const deployment = k8sDeployment.gen(ex, config);
+
+            expect(deployment.metadata.labels.exId).toEqual('e76a0278-d9bc-4d78-bf14-431bcd97528c');
+            expect(deployment.spec.template.spec.affinity).toEqual(yaml.load(`
+                nodeAffinity:
+                  requiredDuringSchedulingIgnoredDuringExecution:
+                    nodeSelectorTerms:
+                    - matchExpressions:
+                      - key: zone
+                        operator: In
+                        values:
+                        - west
+                      - key: region
+                        operator: In
+                        values:
+                        - "42"`));
+        });
+    });
+});
