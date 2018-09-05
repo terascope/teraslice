3c485b747c3b77bd6b4a1355c486e7792fa7c2d1
Austin Godber <godber@uberhip.com>
2018-09-04 17:46:15 -0700

    Change get to update so set will work

--- packages/teraslice-worker/lib/execution-controller/analytics.js
+++ packages/teraslice-worker/lib/execution-controller/analytics.js
@@ -74,7 +74,7 @@ class ExecutionAnalytics {
     }
 
     set(key, value) {
-        _.get(this.executionAnalytics, key, value);
+        _.update(this.executionAnalytics, key, value);
     }
 
     increment(key) {
