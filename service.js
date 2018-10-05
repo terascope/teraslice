b66bbbe01581411aec63fb046a543bad3cd782f0
Austin Godber <godber@uberhip.com>
2018-10-05 16:19:53 -0700

    Handle SIGUSR2 in both cases

--- service.js
+++ service.js
@@ -2,5 +2,12 @@
 
 'use strict';
 
-require('heapdump');
+if (process.env.heapdump === 'true') {
+    require('heapdump');
+    process.on('SIGUSR2', () => console.log('SIGUSR2 received, heapdump enabled'));
+} else {
+    // Sending SIGUSR2 could kill the process under certain conditions if
+    // heapdump is not enabled, this signal handler prevents that
+    process.on('SIGUSR2', () => console.log('SIGUSR2 received, heapdump disabled'));
+}
 require('./packages/teraslice/service.js');
