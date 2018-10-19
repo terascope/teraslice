1ae37bcca644c6b86464986fe60759eb75c70ae1
peterdemartini <thepeterdemartini@gmail.com>
2018-10-19 09:25:00 -0700

    add docs to untils and a few fixes

--- packages/job-components/src/utils.ts
+++ packages/job-components/src/utils.ts
@@ -1,43 +1,59 @@
-export function isString(val: any) {
+/** A simplified implemation of lodash isString */
+export function isString(val: any): val is string {
     return typeof val === 'string';
 }
 
+/** A simplified implemation of lodash isInteger */
+export function isInteger(val: any): val is number {
+    if (typeof val !== 'number') return false;
+
+    return Number.isInteger(val);
+}
+
+/** A native implemation of lodash random */
 export function random(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/** A native implemation of lodash uniq */
 export function uniq<T>(arr: T[]): T[] {
     return [...new Set(arr)];
 }
 
+/** A native implemation of lodash times */ /** A native implemation of lodash times */
 export function times(n: number, fn: (index: number) => any) {
-    let index = -1;
+    let i = -1;
     const result = Array(n);
 
-    while (++index < n) {
-        result[index] = fn(index);
+    while (++i < n) {
+        result[i] = fn(i);
     }
     return result;
 }
 
+/** A native implemation of lodash startsWith */
 export function startsWith(str: string, val: string) {
     if (typeof str !== 'string') return false;
     return str.startsWith(val);
 }
 
+/** A simplified implemation of moment(new Date(val)).isValid() */
 export function isValidDate(val: any): boolean {
     const d = new Date(val);
     // @ts-ignore
     return d instanceof Date && !isNaN(d);
 }
 
-export function flatten<T>(val: List<T[]>): T[] {
+/** A native implemation of lodash flatten */
+export function flatten<T>(val: Many<T[]>): T[] {
     return val.reduce((a, b) => a.concat(b), []);
 }
 
-interface List<T> extends Array<T> {
+/** A simple definitions of array */
+interface Many<T> extends Array<T> {
 }
 
+/** A decorator for locking down a property or method */
 export function locked() {
     // @ts-ignore
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
@@ -47,15 +63,10 @@ export function locked() {
     };
 }
 
+/** A decorator for making a property, or method enumerable */
 export function enumerable(enabled: boolean) {
     // @ts-ignore
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
         descriptor.enumerable = enabled;
     };
-}
-
-export function isInteger(input: any): input is number {
-    if (typeof input !== 'number') return false;
-
-    return Number.isInteger(input);
 }
