b1b6c22b64683206f42acc707fa9fefbcfc2b84e
peterdemartini <thepeterdemartini@gmail.com>
2018-10-22 11:03:51 -0700

    add a more dynamic function for creating a context

--- packages/job-components/src/execution-context/index.ts
+++ packages/job-components/src/execution-context/index.ts
@@ -2,3 +2,32 @@ export * from './api';
 export * from './interfaces';
 export * from './slicer';
 export * from './worker';
+
+import { Context } from '../interfaces';
+import { SlicerExecutionContext } from './slicer';
+import { WorkerExecutionContext } from './worker';
+import {
+    ExecutionContextConfig,
+    WorkerContext,
+    SlicerContext
+} from './interfaces';
+
+export function isWorkerContext(context: Context): context is WorkerContext {
+    return context.assignment === 'worker';
+}
+
+export function isSlicerContext(context: Context): context is SlicerContext {
+    return context.assignment === 'execution_controller';
+}
+
+export function makeExecutionContext(config: ExecutionContextConfig) {
+    if (isSlicerContext(config.context)) {
+        return new SlicerExecutionContext(config);
+    }
+
+    if (isWorkerContext(config.context)) {
+        return new WorkerExecutionContext(config);
+    }
+
+    throw new Error('ExecutionContext requires an assignment of "execution_controller" or "worker"');
+}
