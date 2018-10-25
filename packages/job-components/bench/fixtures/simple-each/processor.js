71d94a6d3132e7aa6b024130628554c69d5506b5
peterdemartini <thepeterdemartini@gmail.com>
2018-10-25 11:24:16 -0700

    make job-component benchmark run in execution context

--- packages/job-components/bench/fixtures/simple-each/processor.js
+++ packages/job-components/bench/fixtures/simple-each/processor.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { EachProcessor } = require('../../dist');
+const { EachProcessor } = require('../../../dist');
 
 class SimpleEach extends EachProcessor {
     constructor(...args) {
