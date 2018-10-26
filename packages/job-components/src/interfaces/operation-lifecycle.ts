2ff7a7b6ca2f05e1685810747ec17c9533bc2460
peterdemartini <thepeterdemartini@gmail.com>
2018-10-26 13:32:03 -0700

    Optimize lifecycle function invocations

--- packages/job-components/src/interfaces/operation-lifecycle.ts
+++ packages/job-components/src/interfaces/operation-lifecycle.ts
@@ -17,34 +17,34 @@ export interface WorkerOperationLifeCycle extends OperationLifeCycle {
      * Called after a slice is initializated, but before the slice
      * has been handed to any operation.
     */
-    onSliceInitialized(sliceId: string): Promise<void>;
+    onSliceInitialized?(sliceId: string): Promise<void>;
 
     /**
      * Called after a the slice is sent to the "Fetcher"
     */
-    onSliceStarted(sliceId: string): Promise<void>;
+    onSliceStarted?(sliceId: string): Promise<void>;
 
     /**
      * Called after a slice is done with the last operation in the execution
     */
-    onSliceFinalizing(sliceId: string): Promise<void>;
+    onSliceFinalizing?(sliceId: string): Promise<void>;
 
     /**
      * Called after the slice has been acknowledged by the "Execution Controller"
     */
-    onSliceFinished(sliceId: string): Promise<void>;
+    onSliceFinished?(sliceId: string): Promise<void>;
 
     /**
      * Called after the slice has been marked as "Failed"
     */
-    onSliceFailed(sliceId: string): Promise <void>;
+    onSliceFailed?(sliceId: string): Promise <void>;
 
     /**
      * Called after the operation failed to process the slice
      * but before the slice is retried.
      * [DEPRECATION NOTICE]: this will be deprecated in near future
     */
-    onSliceRetry(sliceId: string): Promise <void>;
+    onSliceRetry?(sliceId: string): Promise <void>;
 
     /**
      * Called after an operation is complete
@@ -60,23 +60,23 @@ export interface SlicerOperationLifeCycle extends OperationLifeCycle {
      * A method called by the "Execution Controller" to give a "Slicer"
      * the opportunity to track the slices enqueued by the execution controller
     */
-    onSliceEnqueued(slice: Slice): void;
+    onSliceEnqueued?(slice: Slice): void;
 
     /**
      * A method called by the "Execution Controller" to give a "Slicer"
      * the opportunity to track the slices disptached by the execution controller
     */
-    onSliceDispatch(slice: Slice): void;
+    onSliceDispatch?(slice: Slice): void;
 
     /**
      * A method called by the "Execution Controller" to give a "Slicer"
      * the opportunity to track the slices completed by the execution controller
     */
-    onSliceComplete(result: SliceResult): void;
+    onSliceComplete?(result: SliceResult): void;
 
     /**
      * A method called by the "Execution Controller" to give a "Slicer"
      * the opportunity to track various slicer satistics
     */
-    onExecutionStats(stats: ExecutionStats): void;
+    onExecutionStats?(stats: ExecutionStats): void;
 }
