f5fa519622721c1706e95a123bf885c0926fcca5
peterdemartini <thepeterdemartini@gmail.com>
2018-10-26 14:12:40 -0700

    convert delay, noop, save_file and stdout processors to the new style

---
+++ packages/teraslice/test/processors/delay-spec.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const { TestContext, newTestExecutionConfig } = require('@terascope/job-components');
+const Delay = require('../../lib/processors/delay/processor');
+const Schema = require('../../lib/processors/delay/schema');
+
+describe('Delay Processor', () => {
+    const context = new TestContext('delay');
+    const opConfig = { _op: 'delay', ms: 100 };
+    const exConfig = newTestExecutionConfig();
+
+    const delay = new Delay(
+        context,
+        opConfig,
+        exConfig
+    );
+
+    const schema = new Schema(context);
+
+    it('should have a Schema and Processor class', () => {
+        expect(Delay).not.toBeNil();
+        expect(Schema).not.toBeNil();
+    });
+
+    it('should be able to pass validation', () => {
+        const result = schema.validate({ _op: 'delay' });
+        expect(result).toHaveProperty('ms', 100);
+    });
+
+    it('should delay at least 100ms', async () => {
+        const startTime = Date.now();
+        await delay.handle([]);
+        expect(Date.now() - startTime).toBeGreaterThanOrEqual(100);
+    });
+
+    it('should be use a custom delay', async () => {
+        delay.opConfig.ms = 150;
+
+        const startTime = Date.now();
+
+        await delay.handle([]);
+
+        expect(Date.now() - startTime).toBeGreaterThanOrEqual(150);
+    });
+});
