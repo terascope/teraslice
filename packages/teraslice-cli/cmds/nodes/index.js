b230c1de0f927c7349c1dfa19a0913dc1a376b7e
Jeff Montagna <jeff.montagna@gmail.com>
2018-11-15 14:41:48 -0700

    fixed syntax error in nodes/index.js

--- packages/teraslice-cli/cmds/nodes/index.js
+++ packages/teraslice-cli/cmds/nodes/index.js
@@ -3,7 +3,7 @@
 exports.command = 'nodes <command>';
 exports.desc = 'commands to list nodes';
 exports.builder = function builder(yargs) {
-    return yargs.commandDir('.');
-	.demandCommand(2);
+    return yargs.commandDir('.')
+        .demandCommand(2);
 };
 exports.handler = () => {};
