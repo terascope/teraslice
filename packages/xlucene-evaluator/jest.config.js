7604578a429ee691e58802c0037fbcb258ad75a4
peterdemartini <thepeterdemartini@gmail.com>
2019-01-17 13:46:11 -0700

    sync xclune-evaluator with monorepo standards

--- packages/xlucene-evaluator/jest.config.js
+++ packages/xlucene-evaluator/jest.config.js
@@ -1,22 +1,3 @@
 'use strict';
 
-module.exports = {
-    verbose: true,
-    testEnvironment: 'node',
-    setupTestFrameworkScriptFile: 'jest-extended',
-    collectCoverage: true,
-    coverageReporters: ['json', 'lcov', 'text', 'html'],
-    coverageDirectory: 'coverage',
-    testMatch: [
-        '<rootDir>/tests/**/*-spec.{ts,js}',
-        '<rootDir>/tests/*-spec.{ts,js}',
-    ],
-    preset: 'ts-jest',
-    globals: {
-        'ts-jest': {
-            tsConfig: './tsconfig.json',
-            diagnostics: true,
-            pretty: true,
-        }
-    }
-};
+module.exports = require('../../jest.config.base')(__dirname);
