c0dd6d16d7328ccaddb2fc5a6d7f7cc74c344585
Jared Noble <jsnoble1@gmail.com>
2019-01-23 16:00:09 -0700

    added lowercase and uppercase

---
+++ packages/ts-transforms/src/operations/lib/transforms/uppercase.ts
@@ -0,0 +1,27 @@
+
+import _ from 'lodash';
+import { DataEntity } from '@terascope/utils';
+import { OperationConfig } from '../../../interfaces';
+import TransformBase from './base';
+
+export default class Uppercase extends TransformBase {
+    private destination: string;
+    private hasTarget: boolean;
+
+    constructor(config: OperationConfig) {
+        super(config);
+        this.hasTarget = this.target !== this.source;
+        this.destination = this.hasTarget ? this.target : this.source;
+    }
+
+    run(doc: DataEntity): DataEntity | null {
+        const field = _.get(doc, this.source);
+        if (typeof field === 'string') {
+            _.set(doc, this.destination, field.toUpperCase());
+            if (this.hasTarget) _.unset(doc, this.source);
+        } else {
+            _.unset(doc, this.source);
+        }
+        return doc;
+    }
+}
---
+++ packages/ts-transforms/test/operations/transforms/uppercase-spec.ts
@@ -0,0 +1,86 @@
+
+import { Uppercase } from '../../../src/operations';
+import { DataEntity } from '@terascope/utils';
+
+describe('transform operator', () => {
+
+    it('can instantiate', () => {
+        const opConfig = { target_field: 'someField', source_field: 'someField' };
+        expect(() => new Uppercase(opConfig)).not.toThrow();
+    });
+
+    it('can properly throw with bad config values', () => {
+        const badConfig1 = { source_field: 1324 };
+        const badConfig2 = { target_field: [] };
+        const badConfig3 = { source_field: false, target_field: 'someField' };
+        const badConfig4 = {};
+        // @ts-ignore
+        expect(() => new Uppercase(badConfig1)).toThrow();
+        // @ts-ignore
+        expect(() => new Uppercase(badConfig2)).toThrow();
+        // @ts-ignore
+        expect(() => new Uppercase(badConfig3)).toThrow();
+        // @ts-ignore
+        expect(() => new Uppercase(badConfig4)).toThrow();
+    });
+
+    it('can transform strings to Uppercase', () => {
+        const opConfig = { source_field: 'someField' };
+        const test = new Uppercase(opConfig);
+
+        const data1 = new DataEntity({ someField: '56.234,95.234' });
+        const data2 = new DataEntity({});
+        const data3 = new DataEntity({ someField: 'data' });
+        const data4 = new DataEntity({ someField: { some: 'data' } });
+        const data5 = new DataEntity({ someField: false });
+        const data6 = new DataEntity({ someField: 'otHer' });
+        const data7 = new DataEntity({ sideField: 'data' });
+
+        const results1 = test.run(data1);
+        const results2 = test.run(data2);
+        const results3 = test.run(data3);
+        const results4 = test.run(data4);
+        const results5 = test.run(data5);
+        const results6 = test.run(data6);
+        const results7 = test.run(data7);
+
+        expect(DataEntity.isDataEntity(results1)).toEqual(true);
+        expect(results1).toEqual({ someField: '56.234,95.234' });
+        expect(results2).toEqual({});
+        expect(results3).toEqual({ someField: 'DATA' });
+        expect(results4).toEqual({ });
+        expect(results5).toEqual({});
+        expect(results6).toEqual({ someField: 'OTHER' });
+        expect(results7).toEqual({ sideField: 'data' });
+    });
+
+    it('can transform strings to Uppercase to another field', () => {
+        const opConfig = { source_field: 'someField', target_field: 'otherField' };
+        const test = new Uppercase(opConfig);
+
+        const data1 = new DataEntity({ someField: '56.234,95.234' });
+        const data2 = new DataEntity({});
+        const data3 = new DataEntity({ someField: 'data' });
+        const data4 = new DataEntity({ someField: { some: 'data' } });
+        const data5 = new DataEntity({ someField: false });
+        const data6 = new DataEntity({ someField: 'otHer' });
+        const data7 = new DataEntity({ sideField: 'data' });
+
+        const results1 = test.run(data1);
+        const results2 = test.run(data2);
+        const results3 = test.run(data3);
+        const results4 = test.run(data4);
+        const results5 = test.run(data5);
+        const results6 = test.run(data6);
+        const results7 = test.run(data7);
+
+        expect(DataEntity.isDataEntity(results1)).toEqual(true);
+        expect(results1).toEqual({ otherField: '56.234,95.234' });
+        expect(results2).toEqual({});
+        expect(results3).toEqual({ otherField: 'DATA' });
+        expect(results4).toEqual({ });
+        expect(results5).toEqual({});
+        expect(results6).toEqual({ otherField: 'OTHER' });
+        expect(results7).toEqual({ sideField: 'data' });
+    });
+});
