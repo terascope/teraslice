947bedf1f473b7f32199a32f1ba306a42d0dd04d
Jared Noble <jsnoble1@gmail.com>
2019-01-24 13:39:54 -0700

    set explicit matcher and transform exports

--- packages/ts-transforms/src/index.ts
+++ packages/ts-transforms/src/index.ts
@@ -1,24 +1,18 @@
 
-import PhaseManager from './phase_manager';
-import SelectionPhase from './phase_manager/selector_phase';
-import ExtractionPhase from './phase_manager/extraction_phase';
-import PostProcessPhase from './phase_manager/post_process_phase';
-import ValidationPhase from './phase_manager/validation_phase';
-import OutputPhase from './phase_manager/output_phase';
 import Loader from './loader';
-import { OperationsManager } from './operations';
-import ValidationBase from './operations/lib/validations/base';
-import TransformBase from './operations/lib/transforms/base';
+import { OperationsManager, OperationBase, TransformOpBase, ValidationOpBase } from './operations';
+import Transform from './transform';
+import Matcher from './matcher';
+
+export * from './interfaces';
+export * from './phases';
 
 export {
-    PhaseManager,
-    SelectionPhase,
-    ExtractionPhase,
-    PostProcessPhase,
-    ValidationPhase,
-    OutputPhase,
     Loader,
+    Transform,
+    Matcher,
     OperationsManager,
-    ValidationBase,
-    TransformBase
+    ValidationOpBase,
+    TransformOpBase,
+    OperationBase
 };
