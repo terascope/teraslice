d5b2627743ec4399f7a9e42279dfaa2d316742eb
peterdemartini <thepeterdemartini@gmail.com>
2019-01-29 12:58:35 -0700

    Ensure the typescript build configuration works right

--- packages/ts-transforms/bin/ts-transform.js
+++ packages/ts-transforms/bin/ts-transform.js
@@ -7,7 +7,7 @@ const path = require('path');
 try {
     // this path.join is only used for pkg asset injection
     path.join(__dirname, '../package.json');
-    require('../dist/command.js');
+    require('../dist/src/command');
 } catch (err) {
     // eslint-disable-next-line
     console.error('error while attempting to invoke cli command', err.toString());
