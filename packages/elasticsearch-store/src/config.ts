fd24a5c0e160da9268e386e267045bfe46a3609d
peterdemartini <thepeterdemartini@gmail.com>
2019-01-30 11:49:46 -0700

    Fix elasticsearch store to use the new pRetry code

--- packages/elasticsearch-store/src/config.ts
+++ packages/elasticsearch-store/src/config.ts
@@ -1,15 +1,16 @@
 import { PRetryConfig } from '@terascope/utils';
-import { normalizeError } from './utils';
 
 const isTestMode = process.env.NODE_ENV === 'test';
 
 export const MAX_RETRIES = isTestMode ? 2 : 100;
 export const RETRY_DELAY = isTestMode ? 50 : 500;
 
-export function getRetryConfig(): PRetryConfig {
+export function getRetryConfig(): Partial<PRetryConfig> {
     return {
         retries: MAX_RETRIES,
         delay: RETRY_DELAY,
-        normalizeError,
+        matches: [
+            'es_rejected_execution_exception'
+        ]
     };
 }
