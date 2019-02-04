b12847a76234b0f824a67b95c2bd8f1f00e7fde1
Jared Noble <jsnoble1@gmail.com>
2019-02-04 10:01:28 -0700

    temp fix for pathing typescript bug for outside resources

--- packages/xlucene-evaluator/src/lib/lucene-query-parser/index.ts
+++ packages/xlucene-evaluator/src/lib/lucene-query-parser/index.ts
@@ -1,6 +1,14 @@
 
 // @ts-ignore TODO: we should probably add types for this
-import parser from '../../../peg/peg_engine';
+// FIXME: https://github.com/Microsoft/TypeScript/issues/10866
+// I have to change import so that the compiled output works even though
+// it is wrong here. It should be '../../../peg/peg_engine'
+let parser;
+try {
+    parser = require('../../../peg/peg_engine');
+} catch (err) {
+    parser = require('../../../../peg/peg_engine');
+}
 import { AST, AstCallback } from '../interfaces';
 
 export default class LuceneQueryParser {
@@ -12,6 +20,7 @@ export default class LuceneQueryParser {
 
     public parse(luceneStr: string): void {
         try {
+            // @ts-ignore
             this._ast = parser.parse(luceneStr);
         } catch (err) {
             throw new Error(`error occured while attempting to parse lucene query: ${luceneStr} , error: ${err.message}`);
