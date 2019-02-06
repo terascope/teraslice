f0bfacf2c0b3562ce7b496b64f0cf89c33757461
peterdemartini <thepeterdemartini@gmail.com>
2019-02-06 11:22:48 -0700

    use branch-%m.%d-%y for nightly docker releases

--- scripts/docker-release.sh
+++ scripts/docker-release.sh
@@ -25,11 +25,13 @@ build_and_push() {
 
 main() {
     local tag slug version timestamp
+    local branch="$1"
+
     version="$(jq -r '.version' ./packages/teraslice/package.json)"
     timestamp="$(date +%m.%d.%y)"
 
-    if [ "$1" == "nightly" ]; then
-        tag="dev-${timestamp}"
+    if [ -n "$branch" ]; then
+        tag="$branch-${timestamp}"
     else
         tag="v$version"
     fi
