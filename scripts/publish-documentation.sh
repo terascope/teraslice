b5b49a54c326aa85f574d82ca0be8f65cf928969
peterdemartini <thepeterdemartini@gmail.com>
2019-02-08 15:22:28 -0700

    Test github pages deploy

--- scripts/publish-documentation.sh
+++ scripts/publish-documentation.sh
@@ -5,7 +5,9 @@ set -e
 main() {
     cd website \
         && yarn install \
-        && GIT_USER="${GITHUB_NAME}" yarn run publish-gh-pages
+        && GIT_USER="${GITHUB_NAME}" \
+            CURRENT_BRANCH="${TRAVIS_BRANCH:-master}" \
+            yarn run publish-gh-pages
 }
 
 main "$@"
