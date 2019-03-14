1bc9617a8b716868bd9c986d2f73d887e01a549a
Austin Godber <godber@uberhip.com>
2019-03-14 13:46:52 -0700

    shorten job name component of pod name
    @macgyver603 discovered that I was not leaving enough space in my job names to
    guarantee that the job ID made it into the pod name, so I have truncated
    the job name even further

--- packages/teraslice/lib/cluster/services/cluster/backends/kubernetes/k8sResource.js
+++ packages/teraslice/lib/cluster/services/cluster/backends/kubernetes/k8sResource.js
@@ -65,7 +65,7 @@ class K8sResource {
         // name needs to be a valid DNS name since it is used in the svc name,
         // so we can only permit alphanumeric and - characters.  _ is forbidden.
         const jobNameLabel = this.execution.name.replace(/[^a-zA-Z0-9\-.]/g, '-').substring(0, 63);
-        const name = `ts-${this.nameInfix}-${jobNameLabel.substring(0, 42)}-${this.execution.job_id.substring(0, 13)}`;
+        const name = `ts-${this.nameInfix}-${jobNameLabel.substring(0, 35)}-${this.execution.job_id.substring(0, 13)}`;
         const shutdownTimeoutMs = _.get(this.terasliceConfig, 'shutdown_timeout', 60000);
         const shutdownTimeoutSeconds = Math.round(shutdownTimeoutMs / 1000);
 
