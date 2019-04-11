30a83407a8d9e8cc49e62c97360ba4701f9105bd
peterdemartini <thepeterdemartini@gmail.com>
2019-04-11 10:54:17 -0700

    fix coverage results

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,13 +3,14 @@
 set -e
 
 main() {
+    local coverage_dir="$PWD/coverage"
     for package in ./packages/*; do
         pushd "$package";
             yarn test --silent \
                 --detectOpenHandles \
                 --forceExit \
                 --bail \
-                --coverageDirectory="$PWD/coverage" || exit 1;
+                --coverageDirectory="$coverage_dir" || exit 1;
         popd;
     done;
 }
