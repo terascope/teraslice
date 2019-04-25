fbdeea6f47779dd7accebaff9c3cf40f21f76aee
peterdemartini <thepeterdemartini@gmail.com>
2019-04-25 15:06:04 -0700

    Fix tests

--- packages/ui-core/src/IndexApp.test.tsx
+++ packages/ui-core/src/IndexApp.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../src/App';
+import IndexApp from './IndexApp';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(<IndexApp />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
