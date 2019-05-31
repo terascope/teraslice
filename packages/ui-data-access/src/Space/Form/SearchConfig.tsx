a6e29c743c729ff5422ff3092876a5047edc30da
peterdemartini <thepeterdemartini@gmail.com>
2019-05-31 15:02:10 -0700

    Basic Search Config editing

--- packages/ui-data-access/src/Space/Form/SearchConfig.tsx
+++ packages/ui-data-access/src/Space/Form/SearchConfig.tsx
@@ -8,13 +8,13 @@ import FormCheckbox from '../../ModelForm/FormCheckbox';
     index
     connection
     max_query_size
+    require_query
+    sort_enabled
     sort_default
     sort_dates_only
-    sort_enabled
-    default_geo_field
-    preserve_index_name
-    require_query
     default_date_field
+    preserve_index_name
+    default_geo_field
     enable_history
     history_prefix
  */
@@ -39,13 +39,13 @@ const SearchConfig: React.FC<Props> = ({ config, updateConfig }) => {
                     name="index"
                     required
                     label="Search Config Index"
-                    value={config.index}
+                    value={config.index || ''}
                     onChange={onChange}
                 />
                 <Form.Input
                     name="connection"
                     label="Connection to Use"
-                    value={config.connection}
+                    value={config.connection || 'default'}
                     onChange={onChange}
                 />
             </Form.Group>
@@ -61,6 +61,64 @@ const SearchConfig: React.FC<Props> = ({ config, updateConfig }) => {
                     name="require_query"
                     label="Require Query"
                     value={config.require_query}
+                    onChange={onChange}
+                />
+            </Form.Group>
+            <Form.Group as={Segment} basic>
+                <FormCheckbox<SpaceSearchConfig>
+                    name="preserve_index_name"
+                    label="Preserve index name on query results"
+                    value={config.preserve_index_name}
+                    onChange={onChange}
+                />
+            </Form.Group>
+            <Form.Group as={Segment} basic>
+                <Form.Input
+                    name="sort_default"
+                    label="Default Sort Field"
+                    value={config.sort_default || ''}
+                    onChange={onChange}
+                />
+                <FormCheckbox<SpaceSearchConfig>
+                    name="sort_enabled"
+                    label="Allow user to sort"
+                    value={config.sort_enabled}
+                    onChange={onChange}
+                />
+            </Form.Group>
+            <Form.Group as={Segment} basic>
+                <Form.Input
+                    name="default_date_field"
+                    label="Default Date Field"
+                    value={config.default_date_field || ''}
+                    onChange={onChange}
+                />
+                <FormCheckbox<SpaceSearchConfig>
+                    name="sort_dates_only"
+                    label="Allow sorting on date fields only"
+                    value={config.sort_dates_only}
+                    onChange={onChange}
+                />
+            </Form.Group>
+            <Form.Group as={Segment} basic>
+                <Form.Input
+                    name="default_geo_field"
+                    label="Default Geo Field"
+                    value={config.default_geo_field || ''}
+                    onChange={onChange}
+                />
+            </Form.Group>
+            <Form.Group as={Segment} basic>
+                <Form.Input
+                    name="history_prefix"
+                    label="Timeseries index prefix"
+                    value={config.history_prefix || ''}
+                    onChange={onChange}
+                />
+                <FormCheckbox<SpaceSearchConfig>
+                    name="enable_history"
+                    label="Allow users to search timeseries index"
+                    value={config.enable_history}
                     onChange={onChange}
                 />
             </Form.Group>
