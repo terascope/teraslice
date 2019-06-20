7c90b956ae18d8cec333d1c5fcdf8399375b191e
peterdemartini <thepeterdemartini@gmail.com>
2019-06-20 09:53:25 -0700

    Fix .yarn-cache usage in dockerfile

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -128,6 +128,7 @@ cleanup_top_level() {
 post_cleanup() {
     prompt "Do you want to verify/rebuild the node_modules?" &&
         echoerr "* running yarn --force --check-files --update-checksums" &&
+        rm -rf '.yarn-cache' &&
         yarn --force --check-files --update-checksums
 
     prompt "Do you want to rebuild the packages?" &&
