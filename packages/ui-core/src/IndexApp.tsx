3a8d939fa80655f857b593048e8acf9fe14fab47
peterdemartini <thepeterdemartini@gmail.com>
2019-08-12 15:43:13 -0700

    Intercept graphql errors and logout if needed

--- packages/ui-core/src/IndexApp.tsx
+++ packages/ui-core/src/IndexApp.tsx
@@ -1,30 +1,12 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
 import { CoreContextProvider } from '@terascope/ui-components';
 import CoreRouter from './IndexRouter';
 
-const { REACT_APP_DEV_MODE } = process.env;
-
-const apiPath = '/api/v2/data-access';
-const apiUri = REACT_APP_DEV_MODE ? `http://localhost:8000${apiPath}` : apiPath;
-
-const IndexApp: React.FC = () => {
-    const client = new ApolloClient({
-        uri: apiUri,
-        credentials: 'include',
-    });
-
-    // Enable dev tools for apollo client
-    // @ts-ignore
-    window.__APOLLO_CLIENT__ = client;
-
+const IndexApp = () => {
     return (
-        <ApolloProvider client={client}>
-            <CoreContextProvider>
-                <CoreRouter />
-            </CoreContextProvider>
-        </ApolloProvider>
+        <CoreContextProvider>
+            <CoreRouter />
+        </CoreContextProvider>
     );
 };
 
