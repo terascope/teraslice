1cabff08b48cad0fd5f36a3c566b9e068c7505b7
peterdemartini <thepeterdemartini@gmail.com>
2019-08-13 06:42:21 -0700

    Never lose connector errors

--- packages/terafoundation/lib/file_utils.js
+++ packages/terafoundation/lib/file_utils.js
@@ -1,56 +1,75 @@
 'use strict';
 
 const fs = require('fs');
+const { TSError, parseError } = require('@terascope/utils');
 
-function guardedRequire(fileName) {
+function guardedRequire(fileName, errors) {
     try {
         const mod = require(fileName);
         if (mod && mod.default) return mod.default;
         return mod;
-    } catch (e) {
-        if (e.code === 'MODULE_NOT_FOUND') {
+    } catch (error) {
+        if (error.code === 'MODULE_NOT_FOUND') {
             return false;
         }
 
-        throw e;
+        if (Array.isArray(errors)) {
+            errors.push({
+                fileName,
+                error,
+            });
+        }
+        return false;
     }
 }
 
-function getModule(name, obj, err) {
+function getModule(name, obj, reason) {
     let mod;
+
+    // collect the errors
+    const errors = [];
 
     for (const path in obj) {
         if (fs.existsSync(path)) {
-            mod = guardedRequire(path);
+            mod = guardedRequire(path, errors);
             if (!mod) continue;
         }
     }
 
     // check if its a node module
     if (!mod) {
-        mod = guardedRequire(name);
+        mod = guardedRequire(name, errors);
     }
 
     // Still not found check for a connector with underscores
     if (!mod) {
-        mod = guardedRequire(`terafoundation_${name}_connector`);
+        mod = guardedRequire(`terafoundation_${name}_connector`, errors);
     }
 
     // Still not found check for a connector with dashes
     if (!mod) {
-        mod = guardedRequire(`terafoundation-${name}-connector`);
+        mod = guardedRequire(`terafoundation-${name}-connector`, errors);
     }
 
     // Stil not found check for the @terascope namespace
     if (!mod) {
-        mod = guardedRequire(`@terascope/${name}`);
+        mod = guardedRequire(`@terascope/${name}`, errors);
     }
 
-    if (!mod) {
-        throw new Error(err);
+    if (mod) return mod;
+
+    if (errors.length) {
+        const fileNames = errors.map(({ fileName }) => fileName);
+        const messages = errors.map(({ error }) => parseError(error, false));
+        throw new TSError(messages.join(', caused by'), {
+            reason,
+            context: {
+                fileNames,
+            }
+        });
     }
 
-    return mod;
+    throw new TSError(reason);
 }
 
 module.exports = {
