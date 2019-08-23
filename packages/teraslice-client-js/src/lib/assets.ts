efc82f97c957be882d79f9b66aab2ff5713f6b3c
Jared Noble <jsnoble1@gmail.com>
2019-08-23 13:51:28 -0700

    updated assets client

--- packages/teraslice-client-js/src/lib/assets.ts
+++ packages/teraslice-client-js/src/lib/assets.ts
@@ -1,32 +1,76 @@
 
-import { TSError, isEmpty } from '@terascope/job-components';
+import { TSError, isEmpty, isString } from '@terascope/job-components';
+import util from 'util';
 import path from 'path';
 import Client from './client';
-import { QueryOptions, PostData } from '../interfaces';
+import {
+    SearchQuery,
+    PostData,
+    AssetIDResponse,
+    AssetsGetResponse,
+    SearchOptions,
+    RequestOptions,
+    TxtSearchParams,
+    ClientConfig,
+    Asset
+} from '../interfaces';
+
+type GetFn = (name: string) => Promise<Asset[]>;
+
+function deprecateMethod(fn: GetFn) {
+    const msg = 'the "get" method is being deprecated, please use the "getAsset" or "txt" method instead';
+    return util.deprecate(fn, msg);
+}
 
 export default class Assets extends Client {
 
-    async post(data:PostData, options: QueryOptions = {}) {
+    constructor(config: ClientConfig) {
+        super(config);
+        this.get = deprecateMethod(this.get);
+    }
+
+    async post(data: PostData, options: RequestOptions = {}): Promise<AssetIDResponse> {
         if (isEmpty(data)) throw new TSError('Asset stream must not be empty');
-        return super.post('/assets', data, options);
+        const results = await super.post('/assets', data, options);
+        return this.parse(results);
     }
 
-    async delete(id:string) {
+    async delete(id: string, searchOptions: SearchOptions = {}): Promise<AssetIDResponse> {
         if (isEmpty(id)) throw new TSError('Asset delete requires a ID');
-        return super.delete(`/assets/${id}`);
+        const results = await super.delete(`/assets/${id}`, searchOptions);
+        return this.parse(results);
     }
 
-    async list() {
-        return super.get('/assets');
+    async list(query: SearchQuery = {}, searchOptions: SearchOptions = {}):Promise<AssetsGetResponse> {
+        const options = Object.assign({}, searchOptions, { query });
+        return super.get('/assets', options);
     }
 
-    async get(name:string) {
+    async get(name: string): Promise<AssetsGetResponse> {
         const pathing = path.join('/assets', name);
         return super.get(pathing);
     }
 
-    async getAsset(name:string, version:string = '') {
+    async getAsset(name: string, version: string = '', searchOptions: SearchOptions = {}):Promise<AssetsGetResponse> {
+        if (!name || !isString(name)) throw new TSError('name is required, and must be of type string');
+        if (version && !isString(version)) throw new TSError('version if provided must be of type string');
         const pathing = path.join('/assets', name, version);
-        return super.get(pathing);
+        return super.get(pathing, searchOptions);
+    }
+
+    async txt(
+        name: string = '',
+        version: string = '',
+        query: TxtSearchParams = {},
+        searchOptions: SearchOptions = {}
+        ):Promise<AssetsGetResponse> {
+
+        if (name && !isString(name)) throw new TSError('name must be of type string');
+        if (version && !isString(version)) throw new TSError('version must be of type string');
+
+        const options = Object.assign({ json: false }, searchOptions, { query });
+        const pathing = path.join('/txt/assets', name, version);
+
+        return super.get(pathing, options);
     }
 }
