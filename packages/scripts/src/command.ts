3946cba33ed172f99e03106ed2a92e7c8a897340
peterdemartini <thepeterdemartini@gmail.com>
2019-10-03 14:18:48 -0700

    fix: issue when ts-scripts cli fails

--- packages/scripts/src/command.ts
+++ packages/scripts/src/command.ts
@@ -7,7 +7,6 @@ yargs
     .demandCommand(1, 'A command is required. Pass --help to see all available commands and options.')
     .recommendCommands()
     .strict()
-    .showHelpOnFail(true, 'Specify --help for available options')
     .alias('h', 'help')
     .alias('v', 'version')
     .wrap(yargs.terminalWidth()).argv;
