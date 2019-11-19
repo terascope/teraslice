18e89a04a332129665533c2dec3041ee0be72610
Jared Noble <jsnoble1@gmail.com>
2019-11-19 08:07:59 -0700

    change mapping to to support contains query

--- packages/data-types/src/types/versions/v1/geo-json.ts
+++ packages/data-types/src/types/versions/v1/geo-json.ts
@@ -4,7 +4,17 @@ import { ElasticSearchTypes } from '../../../interfaces';
 
 export default class GeoJSON extends BaseType {
     toESMapping(_version?: number) {
-        return { mapping: { [this.field]: { type: 'geo_shape' as ElasticSearchTypes } } };
+        // we need to used depcreated quadtree and strategy becuase CONTAINS is
+        // not yet supported in 6.X or 7.X as of now
+        return {
+            mapping: {
+                [this.field]: {
+                    type: 'geo_shape' as ElasticSearchTypes,
+                    tree: 'quadtree',
+                    strategy: 'recursive'
+                }
+            }
+        };
     }
 
     // TODO: need notion of injecting custom types, what about duplicates
