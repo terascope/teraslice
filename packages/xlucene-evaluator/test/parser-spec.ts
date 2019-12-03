9885fad5ac0528fd96a0051462aa378e2abd420b
Jared Noble <jsnoble1@gmail.com>
2019-12-03 06:50:00 -0700

    added tests for basic parser variable error handling

--- packages/xlucene-evaluator/test/parser-spec.ts
+++ packages/xlucene-evaluator/test/parser-spec.ts
@@ -4,99 +4,119 @@ import allTestCases from './cases/parser';
 import { Parser, ASTType } from '../src';
 
 describe('Parser', () => {
-    // for (const [key, testCases] of Object.entries(allTestCases)) {
-    //     describe(`when testing ${key.replace('_', ' ')} queries`, () => {
-    //         describe.each(testCases)('given query %s', (query, msg, ast, typeConfig) => {
-    //             it(`should be able to parse ${msg}`, () => {
-    //                 const parser = new Parser(query, {
-    //                     type_config: typeConfig
-    //                 });
-    //                 expect(parser.ast).toMatchObject(ast);
-    //             });
-    //         });
-    //     });
-    // }
+    for (const [key, testCases] of Object.entries(allTestCases)) {
+        describe(`when testing ${key.replace('_', ' ')} queries`, () => {
+            describe.each(testCases)('given query %s', (query, msg, ast, typeConfig, variables) => {
+                it(`should be able to parse ${msg}`, () => {
+                    const parser = new Parser(query, {
+                        type_config: typeConfig,
+                        variables
+                    });
+                    expect(parser.ast).toMatchObject(ast);
+                });
+            });
+        });
+    }
 
-    // describe('when testing edge cases', () => {
-    //     describe('given a gigantic query', () => {
-    //         it('should be able to parse it', () => {
-    //             const partOne = times(300, (n) => `a:${n}`).join(' OR ');
-    //             const partTwo = times(200, (n) => `b:${n}`).join(' OR ');
-    //             const partThree = times(500, (n) => `c:${n}`).join(') OR (');
-    //             const parser = new Parser(`(${partOne}) AND ${partTwo} OR (${partThree})`);
-    //             expect(parser.ast).toMatchObject({
-    //                 type: ASTType.LogicalGroup,
-    //             });
-    //         });
-    //     });
-    // });
+    describe('when testing edge cases', () => {
+        describe('given a gigantic query', () => {
+            it('should be able to parse it', () => {
+                const partOne = times(300, (n) => `a:${n}`).join(' OR ');
+                const partTwo = times(200, (n) => `b:${n}`).join(' OR ');
+                const partThree = times(500, (n) => `c:${n}`).join(') OR (');
+                const parser = new Parser(`(${partOne}) AND ${partTwo} OR (${partThree})`);
+                expect(parser.ast).toMatchObject({
+                    type: ASTType.LogicalGroup,
+                });
+            });
+        });
+    });
 
-    // describe('when given a invalid query "(ba"', () => {
-    //     it('should throw an error', () => {
-    //         const errMsg = [
-    //             'Failure to parse xlucene query "(ba",',
-    //             'caused by SyntaxError: Expected ")", field, term, or whitespace but end of input found.',
-    //         ].join(' ');
+    describe('when given a invalid query "(ba"', () => {
+        it('should throw an error', () => {
+            const errMsg = [
+                'Failure to parse xlucene query "(ba",',
+                'caused by SyntaxError: Expected ")", field, term, or whitespace but end of input found.',
+            ].join(' ');
 
-    //         expect(() => {
-    //             new Parser('(ba');
-    //         }).toThrowWithMessage(TSError, errMsg);
-    //     });
-    // });
-
-    // describe('when given a invalid query "AND foo:bar"', () => {
-    //     it('should throw an error', () => {
-    //         const errMsg = 'Failure to parse xlucene query "AND foo:bar", caused by SyntaxError: Expected term, or whitespace but ":" found.';
-    //         expect(() => {
-    //             new Parser('AND foo:bar');
-    //         }).toThrowWithMessage(TSError, errMsg);
-    //     });
-    // });
-
-    // describe('when given a invalid query "OR foo:bar"', () => {
-    //     it('should throw an error', () => {
-    //         const errMsg = 'Failure to parse xlucene query "OR foo:bar", caused by SyntaxError: Expected term, or whitespace but ":" found.';
-    //         expect(() => {
-    //             new Parser('OR foo:bar');
-    //         }).toThrowWithMessage(TSError, errMsg);
-    //     });
-    // });
-
-    // describe('when given a invalid function query "location: something(hello: "world")', () => {
-    //     it('should throw an error', () => {
-    //         const errMsg = 'Failure to parse xlucene query "location: something(hello:"world")", caused by Error: Could not find an xlucene function with name "something"';
-    //         expect(() => {
-    //             new Parser('location: something(hello:"world")');
-    //         }).toThrowWithMessage(TSError, errMsg);
-    //     });
-    // });
-
-    // describe('when given a invalid function query "location:geoBox()", it can still parse syntax but break at validation', () => {
-    //     it('should throw an error', () => {
-    //         const errMsg = 'Failure to parse xlucene query "location:geoBox()", caused by Error: geoBox query needs to specify a "topLeft" parameter';
-    //         expect(() => {
-    //             new Parser('location:geoBox()');
-    //         }).toThrowWithMessage(TSError, errMsg);
-    //     });
-    // });
-
-    // describe('when given a invalid function query "location:geoPolygon(points:[["123.43,223.43", "102.3,123.4"], "99.3,154.4" ])", it can still parse array of arrays but break validation', () => {
-    //     it('should throw an error', () => {
-    //         const errMsg = 'Failure to parse xlucene query "location:geoPolygon(points:[["123.43,223.43", "102.3,123.4"], "99.3,154.4" ])", caused by Error: points parameter must be an array of string values';
-    //         expect(() => {
-    //             new Parser('location:geoPolygon(points:[["123.43,223.43", "102.3,123.4"], "99.3,154.4" ])');
-    //         }).toThrowWithMessage(TSError, errMsg);
-    //     });
-    // });
-
-    describe('when given variables in a query', () => {
-        it('should throw an error if no variables are supplied', () => {
-            const errMsg = 'could not find a variable set with key "$bar"';
             expect(() => {
-                new Parser('foo:$bar');
+                new Parser('(ba');
             }).toThrowWithMessage(TSError, errMsg);
         });
-        // TODO: add tests to fail with array outside fns
-        // TODO: add tests to fail with object variable
+    });
+
+    describe('when given a invalid query "AND foo:bar"', () => {
+        it('should throw an error', () => {
+            const errMsg = 'Failure to parse xlucene query "AND foo:bar", caused by SyntaxError: Expected term, or whitespace but ":" found.';
+            expect(() => {
+                new Parser('AND foo:bar');
+            }).toThrowWithMessage(TSError, errMsg);
+        });
+    });
+
+    describe('when given a invalid query "OR foo:bar"', () => {
+        it('should throw an error', () => {
+            const errMsg = 'Failure to parse xlucene query "OR foo:bar", caused by SyntaxError: Expected term, or whitespace but ":" found.';
+            expect(() => {
+                new Parser('OR foo:bar');
+            }).toThrowWithMessage(TSError, errMsg);
+        });
+    });
+
+    describe('when given a invalid function query "location: something(hello: "world")', () => {
+        it('should throw an error', () => {
+            const errMsg = 'Failure to parse xlucene query "location: something(hello:"world")", caused by Error: Could not find an xlucene function with name "something"';
+            expect(() => {
+                new Parser('location: something(hello:"world")');
+            }).toThrowWithMessage(TSError, errMsg);
+        });
+    });
+
+    describe('when given a invalid function query "location:geoBox()", it can still parse syntax but break at validation', () => {
+        it('should throw an error', () => {
+            const errMsg = 'Failure to parse xlucene query "location:geoBox()", caused by Error: geoBox query needs to specify a "topLeft" parameter';
+            expect(() => {
+                new Parser('location:geoBox()');
+            }).toThrowWithMessage(TSError, errMsg);
+        });
+    });
+
+    describe('when given a invalid function query "location:geoPolygon(points:[["123.43,223.43", "102.3,123.4"], "99.3,154.4" ])", it can still parse array of arrays but break validation', () => {
+        it('should throw an error', () => {
+            const errMsg = 'Failure to parse xlucene query "location:geoPolygon(points:[["123.43,223.43", "102.3,123.4"], "99.3,154.4" ])", caused by TSError: incorrect point given to parse, point:[object Object],[object Object]';
+            expect(() => {
+                new Parser('location:geoPolygon(points:[["123.43,223.43", "102.3,123.4"], "99.3,154.4" ])');
+            }).toThrowWithMessage(TSError, errMsg);
+        });
+    });
+
+    describe('when given variables in a query', () => {
+        it('should throw an error if variables are not supplied', () => {
+            expect(() => {
+                new Parser('foo:$bar');
+            }).toThrow();
+        });
+
+        it('should throw an error if a varaible is an array and is not used in a xlucene function expression', () => {
+            expect(() => {
+                new Parser('foo:$bar', {
+                    variables: {
+                        other: [1, 2, 3]
+                    }
+                });
+            }).toThrow();
+        });
+
+        it('should throw an error if variables values contain an object', () => {
+            expect(() => {
+                // @ts-ignore
+                new Parser('foo:$bar', {
+                    variables: {
+                        bar: { some: 'value' },
+                        other: 'varialbe'
+                    }
+                });
+            }).toThrow();
+        });
     });
 });
