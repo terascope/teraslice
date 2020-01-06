97efc02a2d04f71cea1405a788d4df65dcf5caea
Charlie Iorg <ciorg@terascope.io>
2020-01-06 14:06:52 -0700

    jobs await test

---
+++ packages/teraslice-cli/test/cmds/jobs/await-spec.ts
@@ -0,0 +1,34 @@
+import yargs from 'yargs';
+import errors from '../../../src/cmds/jobs/await';
+
+describe('jobs await', () => {
+    describe('-> parse', () => {
+        it('should parse properly', () => {
+            const yargsCmd = yargs.command(
+                // @ts-ignore
+                errors.command,
+                errors.describe,
+                errors.builder,
+                () => true
+            );
+            const yargsResult = yargsCmd.parse(
+                'await ts-test1', {}
+            );
+            expect(yargsResult.clusterAlias).toEqual('ts-test1');
+        });
+        it('should parse properly with an id specifed', () => {
+            const yargsCmd = yargs.command(
+                // @ts-ignore
+                errors.command,
+                errors.describe,
+                errors.builder,
+                () => true
+            );
+            const yargsResult = yargsCmd.parse(
+                'await ts-test1 99999999-9999-9999-9999-999999999999', {}
+            );
+            expect(yargsResult.clusterAlias).toEqual('ts-test1');
+            expect(yargsResult.id).toEqual('99999999-9999-9999-9999-999999999999');
+        });
+    });
+});
