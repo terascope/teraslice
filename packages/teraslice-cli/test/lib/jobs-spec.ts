670c28330481f549b9e1ffdb68e2fa501f39fe66
Jared Noble <jsnoble1@gmail.com>
2020-01-21 11:23:06 -0700

    fixed weird worker message resolves #1632

--- packages/teraslice-cli/test/lib/jobs-spec.ts
+++ packages/teraslice-cli/test/lib/jobs-spec.ts
@@ -1,26 +1,53 @@
 import path from 'path';
+import nock from 'nock';
+import { pDelay } from '@terascope/utils';
+
 import Jobs from '../../src/helpers/jobs';
 
 describe('jobs', () => {
+    const id = '12341234';
+    const exId = '56785678';
+    const num = 5;
+    const action = 'add';
     let cliArgs;
     let jobs: any;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    let scope: nock.Scope;
+
+    const msg = { message: `${num} workers have been ${action} for execution: ${exId}` };
 
     beforeEach(() => {
         cliArgs = {
             'cluster-manager-type': 'native',
             'output-style': 'txt',
             'config-dir': path.join(__dirname, '../fixtures/config_dir'),
-            'cluster-alias': 'localhost'
+            'cluster-alias': 'localhost',
+            args: { id, action, num }
         };
+        const fn = (obj: any) => {
+            console.log('what is this', obj);
+            return true;
+        };
+
+        scope = nock('http://localhost:5678')
+            .post(`/v1/jobs/${id}/_workers?add=5`, fn)
+            .reply(200, msg);
+
         jobs = new Jobs(cliArgs);
     });
 
     afterEach(() => {
         cliArgs = {};
         jobs = {};
+        nock.cleanAll();
     });
 
-    test('should return a job object', () => {
+    it('should return a job object', () => {
         expect(jobs).toBeDefined();
+    });
+
+    it('can log properly', async () => {
+        const results = await jobs.workers();
+        expect(results).toEqual(`> job: ${id} ${msg.message}`)
     });
 });
