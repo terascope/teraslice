c72ed419471afbe3593392f812ed639f45465fdf
Charlie Iorg <ciorg@terascope.io>
2020-01-29 15:39:56 -0700

    de-data-library

--- packages/datalib/src/validations/field-validator.ts
+++ packages/datalib/src/validations/field-validator.ts
@@ -1,45 +1,92 @@
+import _ from 'lodash';
 import * as ts from '@terascope/utils';
+import ipaddr from 'ipaddr.js';
 import { Repository, ValidationResults } from '../interfaces';
 
-export const respoitory: Repository = {
+export const repository: Repository = {
     isBoolean: { fn: isBoolean, config: {} },
     isBooleanLike: { fn: isBooleanLike, config: {} },
     isEmail: { fn: isEmail, config: {} },
-    isString: { fn: isString, config: {}}
+    isString: { fn: isString, config: {}},
+    validValue: { fn: validValue, config: { }}
 };
 
 
-export function isBoolean(input: string): ValidationResults {
-    const isValid = ts.isBoolean(input);
-    if (isValid) return { isValid };
-    return { isValid, error: `Input: ${input} is not a boolean` };
+export function isBoolean(input: string): boolean {
+    return ts.isBoolean(input);
 }
 
-export function isBooleanLike(input: string): ValidationResults {
-    const isValid = ts.isBooleanLike(input);
-    if (isValid) return { isValid };
-    return { isValid, error: `Input: ${input} is not a booleanLike` };
+export function isBooleanLike(input: string): boolean {
+    return ts.isBooleanLike(input);
 }
 
-export function isEmail(input: string): ValidationResults {
+export function isEmail(input: string): boolean {
     // eslint-disable-next-line
     const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
-    const validString: ValidationResults = isString(input);
-
-    if (!validString.isValid) _invalidValue(input, 'email address must be a string');
-    
-    if (input.toLowerCase().match(regex) != null) return { isValid: true, input };
-    return _invalidValue(input, 'not a valid emailAddress');
+    return isString(input) && input.toLowerCase().match(regex) !== null;
 }
 
 export function isString(input: any): boolean {
     return ts.isString(input);
 }
 
-export function isInteger(input: any) {
-    // should this try to coerse the input to a number? before an int check?
+export function isInteger(input: any, options?: { min?: number, max?: number}): boolean {
+    // should this try to coerce the input to a number? before an int check?
     // like '10' -> 10
+    const isInt = !isBoolean(input) && Number.isInteger(Number(input));
 
-    
+    if (!isInt) return false;
+
+    let minRange = true;
+    let maxRange = true;
+    if(options) {
+        if (options.min != null) {
+            minRange = Number.parseInt(input) > options.min; 
+        }
+
+        if (options.max != null) {
+            maxRange = Number.parseInt(input) < options.max;
+        }
+    }
+
+    return isInt && minRange && maxRange;
+}
+
+export function validValue(input: any, options?: { invalidValues: any[] }): boolean {
+    if (options && options.invalidValues) {
+        return input != null && !options.invalidValues.includes(input);
+    }
+
+    return input != null;
+}
+
+export function isTimestamp(input: any) {
+    // string must be a recognized date format, milliseconds or seconds
+    if (!isInteger(input) || _.isDate(input)) {
+        return !isNaN(Date.parse(input));
+    }
+
+    // if it is a number then it must have at 10 digits or 13
+    return `${input}`.length === 10 || `${input}`.length === 13;
+}
+
+export function isPublicIp(input: any, options?: { private: boolean }) {
+    let range;
+    let result = true;
+
+    try {
+        range = ipaddr.parse(input).range();
+    } catch (e) {
+        return false;
+    }
+
+    // ipv6 private is parsed as uniqueLocal
+    if (range === 'private' || range === 'uniqueLocal') {
+        result = false;
+    }
+
+    if (options && options.private) return !result;
+
+    return result;
 }
--- packages/datalib/test/field_validator-spec.ts
+++ packages/datalib/test/field_validator-spec.ts
@@ -1,1 +1,130 @@
-describe('field validators', () => {});
+import * as valid from '../src/validations/field-validator';
+
+describe('field validators', () => {
+    describe('validValue should', () => {
+        it('validate against null and undefined', () => {
+            expect(valid.validValue(undefined)).toBe(false);
+            expect(valid.validValue(null)).toBe(false);
+            expect(valid.validValue(false)).toBe(true);
+            expect(valid.validValue(324324)).toBe(true);
+            expect(valid.validValue('bob')).toBe(true);
+        });
+
+        it('validate using options.invalidValues', () => {
+            const options = {
+                invalidValues: ['', 'n/a', 'NA', 12345]
+            };
+            expect(valid.validValue('bob', options)).toBe(true);
+            expect(valid.validValue(true, options)).toBe(true);
+            expect(valid.validValue('', options)).toBe(false);
+            expect(valid.validValue('n/a', options)).toBe(false);
+            expect(valid.validValue('NA', options)).toBe(false);
+            expect(valid.validValue(12345, options)).toBe(false);
+        });
+    });
+
+    describe('isInteger should', () => {
+        it('validate ints', () => {
+            expect(valid.isInteger(3)).toBe(true);
+            expect(valid.isInteger('3')).toBe(true);
+            expect(valid.isInteger('0003433')).toBe(true);
+            expect(valid.isInteger('-12321')).toBe(true);
+            expect(valid.isInteger(0)).toBe(true);
+
+            expect(valid.isInteger(3.14159)).toBe(false);
+            expect(valid.isInteger('3.14159')).toBe(false);
+            expect(valid.isInteger(true)).toBe(false);
+            expect(valid.isInteger(['1232', 343])).toBe(false);
+            expect(valid.isInteger(undefined)).toBe(false);
+            expect(valid.isInteger('bob')).toBe(false);
+        });
+
+        it('validate ints with a options', () => {
+            expect(valid.isInteger('3', { min: 0, max: 100 })).toBe(true);
+            expect(valid.isInteger(1032, { min: 100 })).toBe(true);
+            expect(valid.isInteger('12', { max: 100 })).toBe(true);
+            expect(valid.isInteger('-1343', { min: 0, max: 100 })).toBe(false);
+            expect(valid.isInteger('bob', { min: 0, max: 100 })).toBe(false);
+            expect(valid.isInteger(undefined, { min: 0, max: 100 })).toBe(false);
+            expect(valid.isInteger(true, { min: 0 })).toBe(false);
+            expect(valid.isInteger(1032, { max: 100 })).toBe(false);
+        });
+    });
+
+    describe('isTimestamp should', () => {
+        it('validate timestamps', () => {
+             // iso8601 string dates
+            expect(valid.isTimestamp('2019-03-07T23:08:59.673Z')).toBe(true);
+            expect(valid.isTimestamp('2019-03-07')).toBe(true);
+            expect(valid.isTimestamp('2019-03-07T23:08:59')).toBe(true);
+
+            // different string date formats
+            expect(valid.isTimestamp('03/07/2019')).toBe(true);
+            expect(valid.isTimestamp('03-07-2019')).toBe(true);
+            expect(valid.isTimestamp('Jan 12, 2012')).toBe(true);
+            expect(valid.isTimestamp('23 Jan 2012')).toBe(true);
+            expect(valid.isTimestamp('12.03.2012')).toBe(true);
+
+            // timestamp will milliseconds
+            expect(valid.isTimestamp('1552000139673')).toBe(true);
+
+            // timestamp with seconds
+            expect(valid.isTimestamp('1552000139')).toBe(true);
+
+            // date object
+            expect(valid.isTimestamp(new Date())).toBe(true);
+
+            // bad dates
+            expect(valid.isTimestamp('2020-23-09')).toBe(false);
+            expect(valid.isTimestamp('21.03.2012')).toBe(false); 
+            expect(valid.isTimestamp('21/01/2019')).toBe(false);           
+            expect(valid.isTimestamp('123432as;ldkfjasoej293432423')).toBe(false);
+            expect(valid.isTimestamp('1552000        139673')).toBe(false);
+            expect(valid.isTimestamp('unknown')).toBe(false);
+            expect(valid.isTimestamp('1')).toBe(false);
+            expect(valid.isTimestamp('undefined')).toBe(false);
+            expect(valid.isTimestamp(0)).toBe(false);
+            expect(valid.isTimestamp('baddate')).toBe(false);
+            expect(valid.isTimestamp(null)).toBe(false);
+            expect(valid.isTimestamp(undefined)).toBe(false);
+            expect(valid.isTimestamp(true)).toBe(false);
+            expect(valid.isTimestamp(false)).toBe(false);
+            expect(valid.isTimestamp('')).toBe(false);
+            expect(valid.isTimestamp('    ')).toBe(false);
+            // 9 digits
+            expect(valid.isTimestamp('155200013')).toBe(false);
+            // 14 digits
+            expect(valid.isTimestamp('15520001333212')).toBe(false);
+        });
+    });
+
+    describe('isPublicIp should', () => {
+        it('check if an ip is public', () => {
+            // private ips
+            expect(valid.isPublicIp('192.168.0.1')).toBe(false);
+            expect(valid.isPublicIp('fc00:db8::1')).toBe(false);
+
+            // public ips
+            expect(valid.isPublicIp('8.8.8.8')).toBe(true);
+            expect(valid.isPublicIp('2001:db8::1')).toBe(true);
+            expect(valid.isPublicIp('172.194.0.1')).toBe(true);
+
+            // bad ip address
+            expect(valid.isPublicIp('badIpaddress')).toBe(false);
+        });
+
+        it('check if an ip is prive based on options', () => {
+            // private ips
+            expect(valid.isPublicIp('192.168.0.1', { private: true })).toBe(true);
+            expect(valid.isPublicIp('fc00:db8::1', { private: true })).toBe(true);
+
+            // public ips
+            expect(valid.isPublicIp('8.8.8.8', { private: true })).toBe(false);
+            expect(valid.isPublicIp('2001:db8::1', { private: true })).toBe(false);
+            expect(valid.isPublicIp('172.194.0.1', { private: true })).toBe(false);
+
+            // bad ip address
+            expect(valid.isPublicIp('badIpaddress', { private: true })).toBe(false);
+        });
+    });
+});
