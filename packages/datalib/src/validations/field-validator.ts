09da12cb414ded58a65e07e03870a3cc6912b2bc
Charlie Iorg <ciorg@terascope.io>
2020-01-29 12:34:10 -0700

    field validator stuff

--- packages/datalib/src/validations/field-validator.ts
+++ packages/datalib/src/validations/field-validator.ts
@@ -5,7 +5,9 @@ export const respoitory: Repository = {
     isBoolean: { fn: isBoolean, config: {} },
     isBooleanLike: { fn: isBooleanLike, config: {} },
     isEmail: { fn: isEmail, config: {} },
+    isString: { fn: isString, config: {}}
 };
+
 
 export function isBoolean(input: string): ValidationResults {
     const isValid = ts.isBoolean(input);
@@ -22,9 +24,22 @@ export function isBooleanLike(input: string): ValidationResults {
 export function isEmail(input: string): ValidationResults {
     // eslint-disable-next-line
     const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-    if (ts.isString(input)) {
-        if (input.toLowerCase().match(regex)) return { isValid: true };
-        return { isValid: false, error: `Input: ${input} is not a valid email` };
-    }
-    return { isValid: false, error: `Input ${input} must be a string` };
+
+    const validString: ValidationResults = isString(input);
+
+    if (!validString.isValid) _invalidValue(input, 'email address must be a string');
+    
+    if (input.toLowerCase().match(regex) != null) return { isValid: true, input };
+    return _invalidValue(input, 'not a valid emailAddress');
+}
+
+export function isString(input: any): boolean {
+    return ts.isString(input);
+}
+
+export function isInteger(input: any) {
+    // should this try to coerse the input to a number? before an int check?
+    // like '10' -> 10
+
+    
 }
