bd77bf8d92a6e24afcc89723012c0b1d1d583435
Charlie Iorg <ciorg@terascope.io>
2020-01-30 14:34:26 -0700

    added replace, removeIpZoneId, toUnixTime

--- packages/datalib/src/transforms/field-transform.ts
+++ packages/datalib/src/transforms/field-transform.ts
@@ -1,10 +1,14 @@
+import _ from 'lodash';
 import * as ts from '@terascope/utils';
+import * as valid from '../validations/field-validator';
 import { Repository } from '../interfaces';
 
 export const respoitory: Repository = {
     uppercase: { fn: uppercase, config: {} },
     truncate: { fn: truncate, config: { size: { type: 'Int!' } } },
     toBoolean: { fn: toBoolean, config: {} },
+    removeIpZoneId: { fn: removeIpZoneId, config: {} },
+    replace: { fn: replace, config: {} }
 };
 
 export function toBoolean(input: string) {
@@ -23,3 +27,55 @@ export function truncate(input: string, args: ts.AnyObject) {
     if (ts.isString(input)) return input.slice(0, size);
     return null;
 }
+
+export function removeIpZoneId(input: string): string {
+    // removes the zone id from ipv6 addresses
+    // fe80:3438:7667:5c77:ce27%18 -> fe80:3438:7667:5c77:ce27
+    if (input.indexOf('%') > -1) {
+        return input.slice(0, input.indexOf('%'));
+    }
+
+    return input;
+}
+
+export function replace(input: string, args: { searchValue: string, replaceValue: string, global?: boolean, ignoreCase?: boolean }): string {
+    // special regex chars like *, ., [] must be escaped by user inorder to be taken literally
+    const {
+        searchValue,
+        replaceValue,
+        global,
+        ignoreCase
+    } = args;
+
+    let ops = '';
+
+    if (args.global) ops += 'g';
+    if (args.ignoreCase) ops += 'i';   
+
+    try {
+        const re = new RegExp(searchValue, ops);
+        return input.replace(re, replaceValue);
+    } catch (e) {
+        throw new Error(e.message);
+    }
+}
+
+export function toUnixTime(input: any): number {
+    if (!valid.isTimestamp(input)) {
+        throw new Error('Not a valid date, cannot transform to unix time');
+    }
+
+    if (valid.isString(input) && isNaN(input)) {
+        console.log(new Date(input));
+        return Date.parse(input);
+    }
+
+    if (_.isDate(input)) {
+        return input.getTime();
+    }
+
+    if (`${input}`.length === 10) return Number(input) * 1000;
+
+    return Number(input);
+}
+
--- packages/datalib/test/field_transform-spec.ts
+++ packages/datalib/test/field_transform-spec.ts
@@ -1,1 +1,54 @@
-describe('field transforms', () => {});
+import * as transform from '../src/transforms/field-transform';
+
+describe('field transforms', () => {
+    describe('removeIpZoneId should', () => {
+        it('remove zone id and return ip address', () => {
+            expect(transform.removeIpZoneId('8.8.8.8')).toBe('8.8.8.8');
+            expect(transform.removeIpZoneId('172.35.12.18')).toBe('172.35.12.18');
+            expect(transform.removeIpZoneId('2001:db8::1')).toBe('2001:db8::1');
+            expect(transform.removeIpZoneId('fe80::1ff:fe23:4567:890a%eth2')).toBe('fe80::1ff:fe23:4567:890a');
+            expect(transform.removeIpZoneId('2001:DB8::1')).toBe('2001:DB8::1');
+        });
+    });
+
+    describe('replace should', () => {
+        it('find and replace values in string', () => {
+            expect(transform.replace('this-is-a-string', { searchValue: '-', replaceValue: ' ' })).toBe('this is-a-string');
+            expect(transform.replace('this-is-a-string', { searchValue: '-', replaceValue: ' ', global: true })).toBe('this is a string');
+            expect(transform.replace('this-is-a-string', { searchValue: 'can', replaceValue: ' ' })).toBe('this-is-a-string');
+            expect(transform.replace('this*is*a*string', { searchValue: '\\*', replaceValue: '.', global: true })).toBe('this.is.a.string');
+            expect(transform.replace('this CAN be a string', { searchValue: 'can ', replaceValue: '', global: true, ignoreCase: true })).toBe('this be a string');
+        });
+    });
+
+    fdescribe('toUnixTime should', () => {
+        it('convert date iso strings and date objects to unix time', () => { 
+            const testDate = new Date();
+            const unixTime = testDate.getTime();
+            const isoTime = testDate.toISOString();
+
+            expect(transform.toUnixTime(testDate)).toBe(unixTime);
+            expect(transform.toUnixTime(isoTime)).toBe(unixTime);
+            expect(transform.toUnixTime(unixTime)).toBe(unixTime);
+        });
+
+        it('convert date time in seconds to unix time', () => {
+            expect(transform.toUnixTime(1580418907)).toBe(1580418907000);
+            expect(transform.toUnixTime('1580418907')).toBe(1580418907000);
+        });
+
+        it('convert string dates to unix time', () => {
+            expect(transform.toUnixTime('2020-01-01')).toBe(1577836800000);
+            expect(transform.toUnixTime('Jan 1, 2020 UTC')).toBe(1577836800000);
+            expect(transform.toUnixTime('2020 Jan, 1 UTC')).toBe(1577836800000);
+        });
+
+        fit('invalid dates will throw errors', () => {
+            try {
+                transform.toUnixTime('notADate'); 
+            } catch (e) {
+                expect(e.message).toBe('Not a valid date, cannot transform to unix time')
+            }
+        });
+    });
+});
