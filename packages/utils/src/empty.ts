b01a0fb672183711155a142749500f892ee003db
peterdemartini <thepeterdemartini@gmail.com>
2020-03-02 12:11:02 -0700

    add a couple more cases for isEmpty

--- packages/utils/src/empty.ts
+++ packages/utils/src/empty.ts
@@ -12,8 +12,10 @@ export function isNotNil<T>(input: T|Nil): boolean {
 export function isEmpty<T>(val?: T|null|undefined): val is undefined {
     const _val = val as any;
     if (!_val) return true;
+    if (_val instanceof Error) return !_val.message.length;
     if (typeof _val.size === 'number') return !_val.size;
     if (typeof _val.length === 'number') return !_val.length;
+    if (Buffer.isBuffer(_val)) return !_val.toString().length;
     if (typeof val === 'object') return !Object.keys(_val).length;
 
     return true;
