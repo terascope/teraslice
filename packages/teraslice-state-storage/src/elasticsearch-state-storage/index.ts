f925fc965c178b09d5b571a49a2757135b7689e0
peterdemartini <thepeterdemartini@gmail.com>
2020-03-02 13:10:05 -0700

    use the new utils in a couple more places

--- packages/teraslice-state-storage/src/elasticsearch-state-storage/index.ts
+++ packages/teraslice-state-storage/src/elasticsearch-state-storage/index.ts
@@ -4,10 +4,10 @@ import {
     TSError,
     chunk,
     isFunction,
-    pImmediate
+    pImmediate,
+    pMap
 } from '@terascope/utils';
 import esApi, { Client } from '@terascope/elasticsearch-api';
-import { Promise as bPromise } from 'bluebird';
 import { ESStateStorageConfig, MGetCacheResponse } from '../interfaces';
 import CachedStateStorage from '../cached-state-storage';
 
@@ -130,7 +130,7 @@ export default class ESCachedStateStorage {
         const { uncached, duplicates } = this._updateCache(docArray, fn);
         if (uncached.length) {
             // es search for keys not in cache
-            await bPromise.map(uncached, (chunked) => this._esMGet(chunked, fn), {
+            await pMap(uncached, (chunked) => this._esMGet(chunked, fn), {
                 concurrency: this.concurrency
             });
         }
@@ -284,7 +284,7 @@ export default class ESCachedStateStorage {
     private async _esBulkUpdate(docArray: DataEntity[]): Promise<void> {
         const chunked = chunk(docArray, this.chunkSize);
 
-        await bPromise.map(chunked, (chunkedData) => {
+        await pMap(chunked, (chunkedData) => {
             const bulkRequest = this._esBulkUpdatePrep(chunkedData);
             return this.es.bulkSend(bulkRequest);
         }, {
