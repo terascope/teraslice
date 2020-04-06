e677aeaeb58c6a99e230e7db94b93a9515ce968b
Jared Noble <jsnoble1@gmail.com>
2020-04-06 06:07:10 -0700

    added jsdocs to record transforms

--- packages/data-mate/src/transforms/record-transform.ts
+++ packages/data-mate/src/transforms/record-transform.ts
@@ -52,13 +52,21 @@ export const repository: Repository = {
 };
 
 /**
+ * This will set a new key name on a record
  *
+ * @example
+ *
+ * const obj = { hello: 'world' };
+ * const config = { from: 'hello', to: 'goodbye' };
+ * const results = RecordTransform.renameField(cloneDeep(obj), config);
+ * results === { goodbye: 'world' };
  *
  * @export
  * @param {*} record
  * @param {{ from: string; to: string }} args
- * @returns
+ * @returns object
  */
+
 export function renameField(record: any, args: { from: string; to: string }) {
     const { from, to } = args;
     if (!isString(from) || !isString(to)) throw new Error('Invalid parameters, from/to must be supplied be be a string');
@@ -70,13 +78,21 @@ export function renameField(record: any, args: { from: string; to: string }) {
 }
 
 /**
+ * Sets a field on a record
  *
+ * @example
+ *
+ * const obj = { hello: 'world' };
+ * const config = { field: 'other', value: 'stuff' };
+ * const results = RecordTransform.renameField(cloneDeep(obj), config);
+ * results === { hello: 'world', other: 'stuff' };
  *
  * @export
  * @param {*} record
  * @param {{ field: string; value: any }} args
  * @returns
  */
+
 export function setField(record: any, args: { field: string; value: any }) {
     const { field, value } = args;
     if (!isString(field) || value === undefined) throw new Error('Invalid parameters, please set field/value');
@@ -86,13 +102,21 @@ export function setField(record: any, args: { field: string; value: any }) {
 }
 
 /**
+ * removes fields from a record
  *
+ * @example
+ *
+ * const obj = { hello: 'world', other: 'stuff', last: 'thing' };
+ * const config = { fields: ['other', 'last']} ;
+ * const results = RecordTransform.dropFields(cloneDeep(obj), config);
+ * expect(results).toEqual({ hello: 'world' });
  *
  * @export
  * @param {*} record
  * @param {{ fields: string[] }} args
  * @returns
  */
+
 export function dropFields(record: any, args: { fields: string[] }) {
     const { fields } = args;
     if (!fields.every(isString)) throw new Error('Invalid parameters, field must be supplied be be a string');
@@ -105,13 +129,20 @@ export function dropFields(record: any, args: { fields: string[] }) {
 }
 
 /**
+ * Will copy a field to another field
  *
+ * @example
+ * const obj = { hello: 'world', other: 'stuff' };
+ * const config = { from: 'other', to: 'myCopy' };
+ * const results = RecordTransform.copyField(cloneDeep(obj), config);
+ * expect(results).toEqual({ hello: 'world', other: 'stuff', myCopy: 'stuff' });
  *
  * @export
  * @param {*} record
  * @param {{ from: string; to: string }} args
  * @returns
  */
+
 export function copyField(record: any, args: { from: string; to: string }) {
     const { from, to } = args;
     if (!isString(from) || !isString(to)) throw new Error('Invalid parameters, field/copyTo must be supplied be be a string');
