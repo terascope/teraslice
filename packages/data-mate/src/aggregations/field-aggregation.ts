b3d07f4607855e1a81449a08af110dca0eff6f37
Jared Noble <jsnoble1@gmail.com>
2020-04-30 12:46:37 -0700

    first pass on field aggregations

--- packages/data-mate/src/aggregations/field-aggregation.ts
+++ packages/data-mate/src/aggregations/field-aggregation.ts
@@ -15,6 +15,30 @@ export const repository: Repository = {
         config: {},
         output_type: 'Integer' as AvailableType,
         primary_input_type: InputType.Array
+    },
+    sumField: {
+        fn: sumField,
+        config: {},
+        output_type: 'Integer' as AvailableType,
+        primary_input_type: InputType.Array
+    },
+    avgField: {
+        fn: avgField,
+        config: {},
+        output_type: 'Integer' as AvailableType,
+        primary_input_type: InputType.Array
+    },
+    minField: {
+        fn: minField,
+        config: {},
+        output_type: 'Integer' as AvailableType,
+        primary_input_type: InputType.Array
+    },
+    maxField: {
+        fn: maxField,
+        config: {},
+        output_type: 'Integer' as AvailableType,
+        primary_input_type: InputType.Array
     }
 };
 
@@ -31,25 +55,51 @@ export function countField(input: any, _parentContext?: any) {
 }
 
 export function sumField(input: any, _parentContext?: any) {
-    if (ts.isNil(input)) return 0;
-    if (isArray(input)) return input.length;
-    return 1;
+    if (ts.isNil(input)) return null;
+    if (isArray(input)) {
+        return input
+            .filter(ts.isNumber)
+            .reduce((prev, curr) => prev + curr, 0);
+    }
+
+    if (ts.isNumber(input)) return input;
+    return null;
 }
 
 export function avgField(input: any, _parentContext?: any) {
-    if (ts.isNil(input)) return 0;
-    if (isArray(input)) return input.length;
-    return 1;
+    if (ts.isNil(input)) return null;
+    if (isArray(input)) {
+        const numbers = input
+            .filter(ts.isNumber);
+
+        const { length } = numbers;
+
+        if (length === 0) return 0;
+        return numbers.reduce((prev, curr) => prev + curr, 0) / length;
+    }
+
+    if (ts.isNumber(input)) return input;
+    return null;
 }
 
 export function minField(input: any, _parentContext?: any) {
-    if (ts.isNil(input)) return 0;
-    if (isArray(input)) return input.length;
-    return 1;
+    if (ts.isNil(input)) return null;
+    if (isArray(input)) {
+        const numbers = input.filter(ts.isNumber);
+        return Math.min.apply(null, numbers);
+    }
+
+    if (ts.isNumber(input)) return input;
+    return null;
 }
 
 export function maxField(input: any, _parentContext?: any) {
-    if (ts.isNil(input)) return 0;
-    if (isArray(input)) return input.length;
-    return 1;
+    if (ts.isNil(input)) return null;
+    if (isArray(input)) {
+        const numbers = input.filter(ts.isNumber);
+        return Math.max.apply(null, numbers);
+    }
+
+    if (ts.isNumber(input)) return input;
+    return null;
 }
--- packages/data-mate/test/field-aggregator-spec.ts
+++ packages/data-mate/test/field-aggregator-spec.ts
@@ -37,21 +37,88 @@ describe('FieldAggregator', () => {
         });
     });
 
-    describe('countField should', () => {
-        it('return how many values are in an array', () => {
-            expect(FieldAggregator.countField([1, 2, 3])).toEqual(3);
-            expect(FieldAggregator.countField(['1', '2', '2', '3', '3', '3'])).toEqual(6);
+    describe('sumField should', () => {
+        it('will return the sum of all numbers', () => {
+            expect(FieldAggregator.sumField([1, 2, 3])).toEqual(6);
+            expect(FieldAggregator.sumField(['1', '2', '2', '3', '3', '3'])).toEqual(0);
+            expect(FieldAggregator.sumField([1, 'hello', 2, 3, '4', null])).toEqual(6);
         });
 
-        it('return 0 if input is null/undefined', () => {
-            expect(FieldAggregator.countField(null)).toEqual(0);
-            expect(FieldAggregator.countField(undefined)).toEqual(0);
+        it('return null if input is null/undefined', () => {
+            expect(FieldAggregator.sumField(null)).toEqual(null);
+            expect(FieldAggregator.sumField(undefined)).toEqual(null);
         });
 
-        it('return 1 if an array is not passed', () => {
-            expect(FieldAggregator.countField('hello')).toEqual(1);
-            expect(FieldAggregator.countField(1234)).toEqual(1);
-            expect(FieldAggregator.countField({ hello: 'world' })).toEqual(1);
+        it('only returns a number if an array is not passed', () => {
+            expect(FieldAggregator.sumField('hello')).toEqual(null);
+            expect(FieldAggregator.sumField(1234)).toEqual(1234);
+            expect(FieldAggregator.sumField({ hello: 'world' })).toEqual(null);
+        });
+    });
+
+    describe('avgField should', () => {
+        it('will return the average of all numbers', () => {
+            expect(FieldAggregator.avgField([1, 2, 3])).toEqual(2);
+        });
+
+        it('ignore non number items in an array', () => {
+            expect(FieldAggregator.avgField(['1', '1', '2', '3', '3'])).toEqual(0);
+            expect(FieldAggregator.avgField([1, null, 2, { other: 'stuff' }, 3])).toEqual(2);
+        });
+
+        it('return null if input is null/undefined', () => {
+            expect(FieldAggregator.avgField(null)).toEqual(null);
+            expect(FieldAggregator.avgField(undefined)).toEqual(null);
+        });
+
+        it('only return a number if an array is not passed', () => {
+            expect(FieldAggregator.avgField('hello')).toEqual(null);
+            expect(FieldAggregator.avgField(1234)).toEqual(1234);
+            expect(FieldAggregator.avgField({ hello: 'world' })).toEqual(null);
+        });
+    });
+
+    describe('minField should', () => {
+        it('will return the min of all numbers', () => {
+            expect(FieldAggregator.minField([1, 2, 3])).toEqual(1);
+        });
+
+        it('ignore non number items in an array', () => {
+            expect(FieldAggregator.minField(['1', '1', '2', '3', '3'])).toEqual(Infinity);
+            expect(FieldAggregator.minField([1, null, 2, { other: 'stuff' }, 3])).toEqual(1);
+        });
+
+        it('return null if input is null/undefined', () => {
+            expect(FieldAggregator.minField(null)).toEqual(null);
+            expect(FieldAggregator.minField(undefined)).toEqual(null);
+        });
+
+        it('only return a number if an array is not passed', () => {
+            expect(FieldAggregator.minField('hello')).toEqual(null);
+            expect(FieldAggregator.minField(1234)).toEqual(1234);
+            expect(FieldAggregator.minField({ hello: 'world' })).toEqual(null);
+        });
+    });
+
+    describe('maxField should', () => {
+        it('will return the min of all numbers', () => {
+            expect(FieldAggregator.maxField([1, 2, 3])).toEqual(3);
+        });
+
+        it('ignore non number items in an array', () => {
+            expect(FieldAggregator.maxField(['1', '1', '2', '3', '3'])).toEqual(-Infinity);
+            expect(FieldAggregator.maxField([1, null, 2, { other: 'stuff' }, 3])).toEqual(3);
+        });
+
+        it('return null if input is null/undefined', () => {
+            expect(FieldAggregator.maxField(null)).toEqual(null);
+            expect(FieldAggregator.maxField(undefined)).toEqual(null);
+        });
+
+        it('only return a number if an array is not passed', () => {
+            expect(FieldAggregator.maxField('hello')).toEqual(null);
+            expect(FieldAggregator.maxField(1234)).toEqual(1234);
+            expect(FieldAggregator.maxField({ hello: 'world' })).toEqual(null);
         });
     });
 });
