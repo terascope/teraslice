6482f3c45d39b088535335ec69e5a678452e1e24
Jared Noble <jsnoble1@gmail.com>
2020-09-04 15:08:13 -0700

    can do a ip range query

--- packages/xlucene-translator/test/cases/translator/index.ts
+++ packages/xlucene-translator/test/cases/translator/index.ts
@@ -2,12 +2,14 @@ import fieldGroup from './field-group';
 import geo from './geo';
 import logicalGroup from './logical-group';
 import termLevel from './term';
+import ipRange from './ip-range';
 
 export * from './interfaces';
 
 export default {
-    term_level: termLevel,
-    geo,
-    field_group: fieldGroup,
-    logical_group: logicalGroup,
+    // term_level: termLevel,
+    // geo,
+    // field_group: fieldGroup,
+    // logical_group: logicalGroup,
+    ip_range: ipRange
 };
