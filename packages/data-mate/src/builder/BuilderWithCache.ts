c415c08dc57fa4cded5a2bae459b561cdf46d367
peterdemartini <thepeterdemartini@gmail.com>
2021-02-08 13:02:00 -0700

    use BigMap in BuilderWithCache

--- packages/data-mate/src/builder/BuilderWithCache.ts
+++ packages/data-mate/src/builder/BuilderWithCache.ts
@@ -1,7 +1,8 @@
+import { BigMap } from '@terascope/utils';
 import { Builder } from './Builder';
 
 export abstract class BuilderWithCache<T = unknown> extends Builder<T> {
-    _cache = new Map<any, T>()
+    _cache = new BigMap<any, T>()
 
     /**
      * Convert a value to the internal in-memory storage format for the Vector
@@ -16,6 +17,7 @@ export abstract class BuilderWithCache<T = unknown> extends Builder<T> {
         indices?: number|Iterable<number>,
     ): T {
         if (this._cache.has(value)) return this._cache.get(value)!;
+
         const result = super.valueFrom(value, indices);
         this._cache.set(value, result);
         return result;
