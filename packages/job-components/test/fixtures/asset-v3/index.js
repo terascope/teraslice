bed3ba26a6e1bba815439a0f1771e139c3e4616b
Jared Noble <jsnoble1@gmail.com>
2021-02-23 16:29:57 -0700

    create and adjust fixtures

---
+++ packages/job-components/test/fixtures/asset-v3/index.js
@@ -0,0 +1,19 @@
+// module.exports = {
+//     /** A list of the operations provided by this asset bundle */
+//     ASSETS: {
+//         /** The key here would be the current file name */
+//         my_processor: {
+//             Processor: MyProcessor,
+//             Schema: MyProcessorSchema,
+//         },
+//         my_reader: {
+//             Fetcher: MyFetcher,
+//             Slicer: MySlicer,
+//             Schema: MyReaderSchema,
+//         },
+//         my_api: {
+//             API: MyAPI,
+//             Schema: MyAPISchema,
+//         },
+//     },
+// }
