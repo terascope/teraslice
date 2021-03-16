4c9112218c4e95aa1691945f9985d3bd2eca584c
peterdemartini <thepeterdemartini@gmail.com>
2021-03-16 07:51:08 -0700

    update dependencies

--- packages/scripts/src/helpers/sync/configs.ts
+++ packages/scripts/src/helpers/sync/configs.ts
@@ -50,6 +50,7 @@ export async function generateTSConfig(
             }
         },
         include: [],
+        exclude: ['**/node_modules', '**/.*/'],
         // these project references should be ordered by dependents first
         references
     };
