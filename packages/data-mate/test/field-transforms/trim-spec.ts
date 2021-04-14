0f091b421f1a48a2f55b73779746fa91bae2f7a7
Jared Noble <jsnoble1@gmail.com>
2021-04-14 11:32:19 -0700

    trim now only removes whitespace, integrated it with data-frame fn-configs

--- packages/data-mate/test/field-transforms/trim-spec.ts
+++ packages/data-mate/test/field-transforms/trim-spec.ts
@@ -5,11 +5,12 @@ import {
     isEmpty, isNotNil, withoutNil,
 } from '@terascope/utils';
 import {
-    FieldType, Maybe, GeoPointInput, GeoPoint
+    FieldType, Maybe, DataTypeConfig
 } from '@terascope/types';
+import { LATEST_VERSION } from '@terascope/data-types';
 import {
-    functionConfigRepository, functionAdapter, FunctionDefinitionType,
-    ProcessMode, Column, dateFrameAdapter
+    functionConfigRepository, FunctionDefinitionType,
+    ProcessMode, Column, dateFrameAdapter, DataFrame, VectorType
 } from '../../src';
 import { ColumnTests, RowsTests } from '../interfaces';
 
@@ -45,23 +46,67 @@ describe('trimConfig', () => {
         });
     });
 
-    fit('can trim chars', () => {
-        const values = [
-            ' hello ',
-            '   .*left',
-            'right  .* ',
-            '.*.*.*.*',
-        ];
-        const expected = [
-            ' hello ',
-            'left',
-            'right  ',
-            '',
-        ];
-        const toGeoPoint = trimConfig.create({ char: '.*' });
+    describe('can work with dataFrameAdapter', () => {
+        let col: Column<string>;
 
-        values.forEach((val, ind) => {
-            expect(toGeoPoint(val)).toEqual(expected[ind]);
+        const values: Maybe<string>[] = [
+            '   other_things         ',
+            'Stuff        ',
+            '      hello',
+            null,
+            'Spider Man',
+        ];
+        const field = 'someField';
+
+        const frameTestConfig: DataTypeConfig = {
+            version: LATEST_VERSION,
+            fields: {
+                [field]: {
+                    type: FieldType.String
+                },
+                num: {
+                    type: FieldType.Number
+                }
+            }
+        };
+
+        const frameData = values.map((str, ind) => ({ [field]: str as string, num: ind }));
+
+        it('should be able to transform a column using trim', () => {
+            col = Column.fromJSON<string>(field, {
+                type: FieldType.String
+            }, values);
+            const api = dateFrameAdapter(trimConfig);
+            const newCol = api.column(col);
+
+            expect(newCol.toJSON()).toEqual([
+                'other_things',
+                'Stuff',
+                'hello',
+                undefined,
+                'Spider Man',
+            ]);
+        });
+
+        it('should be able to transform a dataFrame using trim', () => {
+            const frame = DataFrame.fromJSON(frameTestConfig, frameData);
+
+            const api = dateFrameAdapter(trimConfig, { field });
+            const newFrame = api.frame(frame);
+
+            expect(newFrame.toJSON()).toEqual([
+                { [field]: 'other_things', num: 0 },
+                { [field]: 'Stuff', num: 1 },
+                { [field]: 'hello', num: 2 },
+                { num: 3 },
+                { [field]: 'Spider Man', num: 4 },
+            ]);
+
+            const [type] = newFrame.columns
+                .filter((column) => column.name === field)
+                .map((column) => column.vector.type);
+
+            expect(type).toEqual(VectorType.String);
         });
     });
 });
