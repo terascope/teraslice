b0d7764ea62c015300e44b8071b21ad1c8f2ebf3
Charlie Iorg <ciorg@terascope.io>
2021-04-29 13:58:40 -0700

    added MIME type description

--- packages/data-mate/src/function-configs/string/isMimeType.ts
+++ packages/data-mate/src/function-configs/string/isMimeType.ts
@@ -6,7 +6,7 @@ export const isMIMETypeConfig: FieldValidateConfig = {
     name: 'isMIMEType',
     type: FunctionDefinitionType.FIELD_VALIDATION,
     process_mode: ProcessMode.INDIVIDUAL_VALUES,
-    description: 'Checks to see if input is a valid Media or MIME Type, used by browsers to determine how to process a URL',
+    description: 'Checks to see if input is a valid Media or MIME (Multipurpose Internet Mail Extensions) Type ',
     create() { return isMIMEType; },
     accepts: [FieldType.String],
 };
