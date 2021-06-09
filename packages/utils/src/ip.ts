ec9e9ba64f22340d97fd550293d751bbb6f31ea1
Jared Noble <jsnoble1@gmail.com>
2021-06-09 08:42:21 -0700

    more ip cleanup

--- packages/utils/src/ip.ts
+++ packages/utils/src/ip.ts
@@ -3,7 +3,7 @@ import ipaddr, { IPv4, IPv6 } from 'ipaddr.js';
 import { parse, stringify } from 'ip-bigint';
 import ip6addr from 'ip6addr';
 import validateCidr from 'is-cidr';
-import { isString, primitiveToString } from './strings';
+import { isString } from './strings';
 import { toInteger, isNumberLike, toBigIntOrThrow } from './numbers';
 import { getTypeOf } from './deps';
 
@@ -22,13 +22,11 @@ export function isIPRangeOrThrow(input: unknown): string {
 
 /** Will throw if input is not a valid IP */
 export function isIPOrThrow(input: unknown): string {
-    const ipValue = primitiveToString(input);
-
-    if (!isString(input) || !isIP(ipValue)) {
-        throw new TypeError(`Expected ${ipValue} (${getTypeOf(input)}) to be a valid IP`);
+    if (!isString(input) || !isIP(input)) {
+        throw new TypeError(`Expected ${input} (${getTypeOf(input)}) to be a valid IP`);
     }
 
-    return ipValue;
+    return input;
 }
 
 export function isIPV6(input: unknown): boolean {
