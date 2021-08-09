68705593bb763175606a1514de6f89d6a4784a48
peterdemartini <thepeterdemartini@gmail.com>
2021-08-09 10:17:51 -0700

    Remove BuilderWithCache since that creates more overhead than it is worth

--- packages/data-mate/src/builder/ListBuilder.ts
+++ packages/data-mate/src/builder/ListBuilder.ts
@@ -1,11 +1,10 @@
 import { coerceToType } from '@terascope/utils';
 import { Maybe } from '@terascope/types';
-import { BuilderOptions } from './Builder';
+import { BuilderOptions, Builder } from './Builder';
 import { VectorType } from '../vector';
 import { WritableData } from '../core';
-import { BuilderWithCache } from './BuilderWithCache';
 
-export class ListBuilder<T = unknown> extends BuilderWithCache<readonly Maybe<T>[]> {
+export class ListBuilder<T = unknown> extends Builder<readonly Maybe<T>[]> {
     _valueFrom = coerceToType<readonly Maybe<T>[]>(this.config, this.childConfig);
 
     constructor(
