c314ce8d32c7a3618c66c701458159ab5657af7e
peterdemartini <thepeterdemartini@gmail.com>
2021-08-20 13:49:19 -0700

    Add ListVector.getRowIterator

--- packages/data-mate/test/vector/list-vector-spec.ts
+++ packages/data-mate/test/vector/list-vector-spec.ts
@@ -1,8 +1,10 @@
 import 'jest-fixtures';
-import { bigIntToJSON, isNotNil, toString } from '@terascope/utils';
+import {
+    bigIntToJSON, isNotNil, times, toString
+} from '@terascope/utils';
 import { FieldType } from '@terascope/types';
 import {
-    Vector, Builder, WritableData
+    Vector, Builder, WritableData, ListVector
 } from '../../src';
 
 describe('ListVector', () => {
@@ -17,7 +19,7 @@ describe('ListVector', () => {
     ];
 
     describe.each(testCases)('when field type is %s', (type, input, output) => {
-        let vector: Vector<any>;
+        let vector: ListVector<any>;
         let expected: any[];
         beforeAll(() => {
             const builder = Builder.make(new WritableData(input.length), {
@@ -25,18 +27,38 @@ describe('ListVector', () => {
             });
             input.forEach((val) => builder.append(val));
 
-            vector = builder.toVector();
-            expected = (output ?? input).map((val) => {
+            vector = builder.toVector() as ListVector<any>;
+            const convert = (val: unknown): unknown => {
+                if (Array.isArray(val)) return val.map(convert);
                 if (typeof val === 'bigint') {
                     return bigIntToJSON(val);
                 }
                 if (val == null) return undefined;
                 return val;
-            });
+            };
+            expected = (output ?? input).map(convert);
         });
 
         it('should return the correct output', () => {
             expect(vector.toJSON()).toEqual(expected);
+        });
+
+        it('should be able to get each row using getRowIterator without skipNilListValues', () => {
+            const fn = (i: number) => Array.from(vector.getRowIterator(i, true));
+            expect(times(vector.size, fn)).toEqual(expected.map((val) => {
+                if (val == null) return [val];
+                return val;
+            }));
+        });
+
+        it('should be able to get each row using getRowIterator with skipNilListValues', () => {
+            const fn = (i: number) => Array.from(vector.getRowIterator(i, true, {
+                skipNilListValues: true
+            }));
+            expect(times(vector.size, fn)).toEqual(expected.map((val) => {
+                if (val == null) return [];
+                return val.filter((childVal: unknown) => childVal != null);
+            }));
         });
 
         it('should return have the correct size', () => {
