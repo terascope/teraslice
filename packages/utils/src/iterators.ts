c99c4eda9b31dad1e468b26119cc6e1388d4b047
peterdemartini <thepeterdemartini@gmail.com>
2021-08-20 14:07:22 -0700

    Fix iterator check functions

--- packages/utils/src/iterators.ts
+++ packages/utils/src/iterators.ts
@@ -1,13 +1,13 @@
 /**
  * Returns true if the input is an iterator
 */
-export function isIterator(input: unknown): input is Iterable<any> {
+export function isIterator<T>(input: unknown): input is Iterable<T> {
     return Symbol.iterator in Object(input);
 }
 
 /**
  * Returns true if the input is an async iterator
 */
-export function isAsyncIterator(input: unknown): input is AsyncIterable<any> {
+export function isAsyncIterator<T>(input: unknown): input is AsyncIterable<T> {
     return Symbol.asyncIterator in Object(input);
 }
