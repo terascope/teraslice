6eed1c2dd7563f5121b00ac29f7abde400eb11d1
peterdemartini <thepeterdemartini@gmail.com>
2021-10-06 14:11:44 -0700

    Improve data frame search benchmark

--- packages/data-mate/bench/data-frame-search-suite.js
+++ packages/data-mate/bench/data-frame-search-suite.js
@@ -20,6 +20,30 @@ const run = async () => {
         }
     });
 
+    suite.add('using query: "birthday:>=\\"1990-01-04T00:00:00.000Z\\" AND alive:true"', {
+        fn() {
+            dataFrame.search('birthday:>="1990-01-04T00:00:00.000Z" AND alive:true');
+        }
+    });
+
+    suite.add('using query: "ip:\\"192.198.0.0/24\\"', {
+        fn() {
+            dataFrame.search('ip:"192.198.0.0/24"');
+        }
+    });
+
+    suite.add('using query: "location:geoDistance(point: "33, 26", distance:1000mi)"', {
+        fn() {
+            dataFrame.search('location:geoDistance(point: "33, 26", distance:1000mi)');
+        }
+    });
+
+    suite.add('using query: "metadata.number_of_friends:>5"', {
+        fn() {
+            dataFrame.search('metadata.number_of_friends:>5');
+        }
+    });
+
     return suite.run({
         async: true,
         initCount: 2,
