23d1ab787706aecd6ebd4c557c790175bff087b4
Jared Noble <jsnoble1@gmail.com>
2022-10-28 15:24:37 -0700

    got tests to run with scripts

--- packages/xlucene-parser/scripts/generate-engine.js
+++ packages/xlucene-parser/scripts/generate-engine.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
-
+import { fileURLToPath } from 'url';
 import fs from 'fs';
 import path from 'path';
 import peg from 'peggy';
 import tspegjs from 'ts-pegjs';
 
-export default function generate() {
-    const input = path.join(__dirname, '..', 'peg', 'lucene.pegjs');
-    const output = path.join(__dirname, '..', 'src/index.js', 'peg-engine.ts');
+const dirPath = fileURLToPath(new URL('.', import.meta.url));
+
+export default function generateEngine() {
+    const input = path.join(dirPath, '..', 'peg', 'lucene.pegjs');
+    const output = path.join(dirPath, '..', 'src', 'peg-engine.ts');
 
     const current = fs.existsSync(output) && fs.readFileSync(output, 'utf8');
     const grammar = fs.readFileSync(input, 'utf8');
@@ -16,10 +18,10 @@ export default function generate() {
         optimize: 'speed',
         plugins: [tspegjs],
         parser: {},
-        format: 'es',
+        format: 'commonjs',
         tspegjs: {
             noTslint: true,
-            customHeader: "import { makeContext } from './context';\nimport * as i from './interfaces.js';\nimport { xLuceneFieldType } from '@terascope/types';"
+            customHeader: "import { makeContext } from './context.js';\nimport * as i from './interfaces.js';\nimport { xLuceneFieldType } from '@terascope/types';"
         },
     });
 
@@ -28,9 +30,11 @@ export default function generate() {
     return output;
 }
 
-// if (require.main === module) {
-//     const outputFile = generate();
-//     if (outputFile) {
-//         console.error(`* generated ${path.relative(process.cwd(), outputFile)}`);
-//     }
-// }
+// we execute if used as a script
+if (import.meta.url.startsWith('file:')) {
+    const outputFile = generateEngine();
+
+    if (outputFile) {
+        console.error(`* generated ${path.relative(process.cwd(), outputFile)}`);
+    }
+}
