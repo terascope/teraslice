6d3c9059be19d3049f0a1adab145a8e9ffb7acea
sotojn <jsoto@terascope.com>
2023-09-13 13:58:19 -0700

    fix small bug in bump assets tests

--- packages/scripts/test/bump-spec.ts
+++ packages/scripts/test/bump-spec.ts
@@ -677,7 +677,7 @@ describe('Bump Assets', () => {
                         name: 'package-2',
                         version: '1.0.0',
                         dependencies: {
-                            'package-1': '^2.1.1'
+                            'package-1': '^2.2.0'
                         },
                         devDependencies: {
                         },
