0c72f9d3f38d27be5d491e49f109a9dee28dc735
busma13 <pluitjens@terascope.io>
2023-10-20 16:58:30 -0700

    Add ignore pattern for testing in k8s

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -2,6 +2,8 @@
 
 const config = require('../jest.config.base')(__dirname);
 
+// FIXME update arrays to run tests specific to platform
+config.testPathIgnorePatterns = process.env.TEST_PLATFORM === 'kubernetes' ? ['data/recovery-spec', 'cluster/worker-allocation-spec', 'cluster/state-spec'] : [];
 config.collectCoverage = false;
 delete config.transform;
 module.exports = config;
