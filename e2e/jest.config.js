e8fbb94b7b47f81e2620f4554d4deec3fe2400d4
Jared Noble <jsnoble1@gmail.com>
2024-01-19 08:52:18 -0700

    disable recovery e2e tests

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -8,4 +8,30 @@ const config = require('../jest.config.base')(__dirname);
 config.testPathIgnorePatterns = process.env.TEST_PLATFORM === 'kubernetes' ? ['data/recovery-spec', 'cluster/worker-allocation-spec', 'cluster/state-spec'] : [];
 config.collectCoverage = false;
 delete config.transform;
+// config.moduleNameMapper = {
+//     '^(\\.{1,2}/.*)\\.js$': '$1',
+// };
+config.transform = {};
+// config.transformIgnorePatterns = [];
+// config.transform['^.+\\.(t|j)sx?$'] = ['@swc/jest', {
+//     jsc: {
+//         loose: true,
+//         parser: {
+//             syntax: 'typescript',
+//             tsx: false,
+//             decorators: true
+//         },
+//         transform: {
+//             legacyDecorator: true,
+//             decoratorMetadata: true
+//         },
+//         target: 'es2022'
+//     },
+//     module: {
+//         type: 'commonjs',
+//         strictMode: false,
+//         noInterop: false
+//     }
+// }];
+
 module.exports = config;
