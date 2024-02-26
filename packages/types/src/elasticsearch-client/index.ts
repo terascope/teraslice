78e7d7da6abba24befea159cccbedf5a47989105
Jared Noble <jsnoble1@gmail.com>
2024-02-26 11:31:39 -0700

    backwards compatible mirgration of es-like types and changes of ClientParams and ClientResponse

--- packages/types/src/elasticsearch-client/index.ts
+++ packages/types/src/elasticsearch-client/index.ts
@@ -1,3 +1,5 @@
 export * as ClientParams from './elasticsearch-params';
 export * as ClientResponse from './elasticsearch-response';
 export * as ESTypes from './elasticsearch-types';
+export * from './elasticsearch-params';
+export * from './elasticsearch-response';
