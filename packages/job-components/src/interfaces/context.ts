0c52570625e3e5c7292e1d97387ea64ba2333440
busma13 <pluitjens@terascope.io>
2024-03-01 16:26:02 -0700

    rename asset_storage_connector to asset_storage_connection

--- packages/job-components/src/interfaces/context.ts
+++ packages/job-components/src/interfaces/context.ts
@@ -63,6 +63,8 @@ export interface TerasliceConfig {
 
 export interface TerafoundationConfig {
     connectors: Record<string, any>;
+    asset_storage_connection?: string;
+    asset_storage_bucket?: string;
 }
 
 export interface SysConfig {
--- packages/terafoundation/src/interfaces.ts
+++ packages/terafoundation/src/interfaces.ts
@@ -101,7 +101,7 @@ export type FoundationSysConfig<S> = {
         log_path: string;
         log_level: LogLevelConfig;
         logging: LogType[];
-        asset_storage_connector?: string;
+        asset_storage_connection?: string;
         asset_storage_bucket?: string;
     };
 } & S;
