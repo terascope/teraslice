0cf1c6e96e382631884ba094ad797abb2fdb3c7d
sotojn <jsoto@terascope.com>
2024-03-15 15:46:33 -0700

    refactor s3Store to use file-asset functions

--- packages/teraslice/src/lib/storage/backends/s3_store.ts
+++ packages/teraslice/src/lib/storage/backends/s3_store.ts
@@ -2,6 +2,12 @@
 import {
     Logger, TSError, isTest, logError, pDelay, pWhile, random
 } from '@terascope/utils';
+/*
+    ******* NOTE *******
+
+    We need to remove the @aws-sdk/client-s3
+    after I figure the rest of this out
+*/
 import {
     BucketAlreadyExists,
     BucketAlreadyOwnedByYou,
@@ -13,7 +19,17 @@ import {
     PutObjectCommand
 } from '@aws-sdk/client-s3';
 import { Context, TerafoundationConfig } from '@terascope/job-components';
-import { createS3Client, S3Client, S3ClientConfig } from '@terascope/file-asset-apis';
+import {
+    createS3Client,
+    S3Client,
+    S3ClientConfig,
+    s3RequestWithRetry,
+    listS3Objects,
+    putS3Object,
+    getS3Object,
+    deleteS3Object,
+    createS3Bucket
+} from '@terascope/file-asset-apis';
 import { HttpHandlerOptions } from '@smithy/types';
 import { makeLogger } from '../../workers/helpers/terafoundation.js';
 
@@ -52,14 +68,27 @@ export class S3Store {
 
     async initialize() {
         this.api = await createS3Client(this.terafoundation.connectors.s3[this.connector] as S3ClientConfig);
-        const input = {
+        // const input = {
+        //     Bucket: this.bucket
+        // };
+        // const command = new CreateBucketCommand(input);
+        const command = {
             Bucket: this.bucket
         };
-        const command = new CreateBucketCommand(input);
 
         await pWhile(async () => {
             try {
-                await this.api.send(command);
+                // await this.api.send(command);
+
+                /*
+                    ******* NOTE *******
+
+                    There is also a `doesBucketExist` method in
+                    the s3Helpers. Maybe consider using it here?
+
+                */
+                const client = this.api;
+                createS3Bucket(client, command);
                 const isReady = await this.verifyClient();
                 return isReady;
             } catch (err) {
@@ -87,14 +116,18 @@ export class S3Store {
     }
 
     async get(recordId: string) {
-        const command = new GetObjectCommand({
+        const command = {
             Bucket: this.bucket,
             Key: `${recordId}.zip`
-        });
+        };
         try {
             this.logger.trace(`getting record with id: ${recordId} from ${this.bucket} bucket`);
-
-            const response = await this.api.send(command);
+            const client = this.api;
+            const response = await s3RequestWithRetry({
+                client,
+                func: getS3Object,
+                params: command
+            });
             const s3File = await response.Body?.transformToString('base64');
             if (typeof s3File !== 'string') {
                 throw new TSError(`Unable to get asset ${recordId} from s3`);
@@ -114,16 +147,34 @@ export class S3Store {
         try {
             this.logger.trace(`saving record id: ${recordId} to ${this.bucket} bucket`);
 
-            const command = new PutObjectCommand({
+            // const command2 = new PutObjectCommand({
+            //     Bucket: this.bucket,
+            //     Key: `${recordId}.zip`,
+            //     Body: data
+            // });
+            const command = {
                 Bucket: this.bucket,
                 Key: `${recordId}.zip`,
                 Body: data
-            });
+            };
             const options: HttpHandlerOptions = {
                 // FIXME: this is only related to time it takes to connect, not get a response
                 requestTimeout: timeout
             };
-            await this.api.send(command, options);
+            const client = this.api;
+            await s3RequestWithRetry({
+                client,
+                func: putS3Object,
+                params: command
+            });
+            /*
+                ******* FIXME *******
+
+                await this.api.send(command, options);
+
+                This used to return `PutObjectCommandOutput`
+                but now returns GetObjectCommandOutput
+            */
         } catch (err) {
             throw new TSError(`Error saving asset to S3: ${err}`);
         }
@@ -133,11 +184,29 @@ export class S3Store {
         try {
             this.logger.trace(`removing record ${recordId} from ${this.bucket} bucket`);
 
-            const command = new DeleteObjectCommand({
+            // const command = new DeleteObjectCommand({
+            //     Bucket: this.bucket,
+            //     Key: `${recordId}.zip`
+            // });
+            // await this.api.send(command);
+
+            /*
+                ******* FIXME *******
+
+                This used to return `DeleteObjectCommandOutput`
+                but now returns GetObjectCommandOutput
+            */
+            const command = {
                 Bucket: this.bucket,
                 Key: `${recordId}.zip`
+            }
+            const client = this.api;
+            await s3RequestWithRetry({
+                client,
+                func: deleteS3Object,
+                params: command
             });
-            await this.api.send(command);
+
         } catch (err) {
             throw new TSError(`Error deleting asset from S3: ${err}`);
         }
@@ -148,29 +217,43 @@ export class S3Store {
         const objectList: Record<string, any>[] = [];
         let nextContinuationToken;
         let continuePagination = true;
-
-        do {
-            const command = new ListObjectsV2Command({
-                Bucket: this.bucket,
-                ContinuationToken: nextContinuationToken || undefined,
-                // MaxKeys: 1000  // Default is 1000
-            });
-            const response: any = await this.api.send(command);
-            response.Contents?.forEach((c: any) => {
-                const s3Record = {
-                    File: c.Key,
-                    Size: c.Size,
-                    // Created: c.LastModified
-                };
-                objectList.push(s3Record);
-            });
-            if (!response.IsTruncated) {
-                continuePagination = false;
-                nextContinuationToken = undefined;
-            } else {
-                nextContinuationToken = response.NextContinuationToken;
-            }
-        } while (continuePagination);
+        try {
+            do {
+                // const command = new ListObjectsV2Command({
+                //     Bucket: this.bucket,
+                //     ContinuationToken: nextContinuationToken || undefined,
+                //     // MaxKeys: 1000  // Default is 1000
+                // });
+                // const response: any = await this.api.send(command);
+                const command = {
+                    Bucket: this.bucket,
+                    ContinuationToken: nextContinuationToken || undefined,
+                    // MaxKeys: 1000  // Default is 1000
+                }
+                const client = this.api;
+                const response: any = await s3RequestWithRetry({
+                    client,
+                    func: listS3Objects,
+                    params: command
+                });
+                response.Contents?.forEach((c: any) => {
+                    const s3Record = {
+                        File: c.Key,
+                        Size: c.Size,
+                        // Created: c.LastModified
+                    };
+                    objectList.push(s3Record);
+                });
+                if (!response.IsTruncated) {
+                    continuePagination = false;
+                    nextContinuationToken = undefined;
+                } else {
+                    nextContinuationToken = response.NextContinuationToken;
+                }
+            } while (continuePagination);
+        } catch (err) {
+            throw new TSError(`Error listing assets from S3: ${err}`);
+        }
         return objectList;
     }
 
@@ -181,12 +264,24 @@ export class S3Store {
     */
     async verifyClient() {
         if (this.isShuttingDown) return false;
-        const command = new ListObjectsV2Command({
+
+        // const command = new ListObjectsV2Command({
+        //     Bucket: this.bucket,
+        //     MaxKeys: 0
+        // });
+
+        const command = {
             Bucket: this.bucket,
             MaxKeys: 0
-        });
+        };
         try {
-            await this.api.send(command);
+            // await this.api.send(command);
+            const client = this.api
+            await s3RequestWithRetry({
+                client,
+                func: listS3Objects,
+                params: command
+            });
             this.logger.trace('Client verification successful');
             return true;
         } catch (err) {
