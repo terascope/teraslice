ebcea650f24ae409e4518fd44998ce817965f704
busma13 <pluitjens@terascope.io>
2024-03-18 15:26:42 -0700

    Add another test.

--- e2e/test/cases/assets/s3_asset-spec.ts
+++ e2e/test/cases/assets/s3_asset-spec.ts
@@ -5,8 +5,11 @@ import { S3Store } from 'teraslice';
 import { MINIO_HOST } from 'e2e/test/config';
 
 if (!process.env.TEST_MINIO) {
+    // If the TEST_MINIO var is not set we are not in
+    // e2e-external-storage-tests, so we do not run tests.
+    // We must run a dummy test or jest will throw an error that no tests were run.
     describe('test to run if not in e2e-external-storage-tests', () => {
-        it('tests are disabled', () => {
+        it('passes if tests are disabled', () => {
             expect(true).toEqual(true);
         });
     });
@@ -42,7 +45,7 @@ if (!process.env.TEST_MINIO) {
                     context,
                     terafoundation: mockTerafoundation,
                     connection: 'default',
-                    bucket: 'tera-assets'
+                    bucket: 'ts-assets'
                 });
 
                 await s3Backend.initialize();
@@ -54,7 +57,7 @@ if (!process.env.TEST_MINIO) {
 
             it('should throw error trying to verify client if bucket does not exist', async () => {
                 const command = new DeleteBucketCommand({
-                    Bucket: 'tera-assets'
+                    Bucket: 'ts-assets'
                 });
                 await s3Backend.api.send(command);
 
@@ -67,19 +70,19 @@ if (!process.env.TEST_MINIO) {
                 expect(response).toEqual(true);
 
                 const command = new DeleteBucketCommand({
-                    Bucket: 'tera-assets'
+                    Bucket: 'ts-assets'
                 });
                 await s3Backend.api.send(command);
             });
         });
 
-        describe('save and remove assets', () => {
+        describe('save, get, and remove assets', () => {
             beforeAll(async () => {
                 s3Backend = new S3Store({
                     context,
                     terafoundation: mockTerafoundation,
                     connection: 'default',
-                    bucket: 'tera-assets'
+                    bucket: 'ts-assets'
                 });
 
                 await s3Backend.initialize();
@@ -87,20 +90,20 @@ if (!process.env.TEST_MINIO) {
 
             afterAll(async () => {
                 const command = new DeleteBucketCommand({
-                    Bucket: 'tera-assets'
+                    Bucket: 'ts-assets'
                 });
                 await s3Backend.api.send(command);
                 await s3Backend.shutdown();
             });
 
-            it('should be able to write in a zip file to tera_assets bucket', async () => {
+            it('should be able to write in a zip file to ts_assets bucket', async () => {
                 const filePath = 'test/fixtures/assets/example_asset_1.zip';
                 await s3Backend.save('ex1', fse.readFileSync(filePath), 1000);
                 const result = await s3Backend.list();
                 expect(result[0].File).toBe('ex1.zip');
             });
 
-            it('should be able to delete zip file in tera_assets bucket', async () => {
+            it('should be able to delete zip file in ts_assets bucket', async () => {
                 await s3Backend.remove('ex1');
 
                 const result = await s3Backend.list();
@@ -115,6 +118,29 @@ if (!process.env.TEST_MINIO) {
 
                 expect(result).toStartWith('UEsDBAo');
                 await s3Backend.remove('ex2');
+            });
+        });
+
+        describe('when bucket name is not defined', () => {
+            afterAll(async () => {
+                const command = new DeleteBucketCommand({
+                    Bucket: 'ts-assets-teracluster'
+                });
+                await s3Backend.api.send(command);
+                await s3Backend.shutdown();
+            });
+
+            it('should create a bucket name containing the terafoundation teraslice name', async () => {
+                s3Backend = new S3Store({
+                    context,
+                    terafoundation: mockTerafoundation,
+                    connection: 'default',
+                    bucket: undefined
+                });
+
+                await s3Backend.initialize();
+
+                expect(s3Backend.bucket).toBe('ts-assets-teracluster');
             });
         });
     });
