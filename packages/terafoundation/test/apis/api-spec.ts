f600d83d0c2b39543e06d5f0a2012b2da4143d95
busma13 <pluitjens@terascope.io>
2024-04-30 10:57:52 -0700

    fix test

--- packages/terafoundation/test/apis/api-spec.ts
+++ packages/terafoundation/test/apis/api-spec.ts
@@ -66,6 +66,7 @@ describe('apis module', () => {
         expect(typeof context.apis.foundation.getSystemEvents).toBe('function');
         expect(typeof context.apis.foundation.getConnection).toBe('function');
         expect(typeof context.apis.foundation.startWorkers).toBe('function');
+        expect(typeof context.apis.foundation.createClient).toBe('function');
         expect(typeof context.apis.foundation.promMetrics).toBe('object');
         expect(typeof context.apis.foundation.promMetrics.init).toBe('function');
         expect(typeof context.apis.foundation.promMetrics.set).toBe('function');
