7eb693ec5273c8dfe359efd51fe9fdfd39f18908
sotojn <jsoto@terascope.com>
2024-05-23 15:09:44 -0700

    remove experiential json imports

---
+++ e2e/test/fixtures/jobs/reindex.ts
@@ -0,0 +1,24 @@
+export default {
+    name: 'Reindex',
+    lifecycle: 'once',
+    workers: 1,
+    analytics: true,
+    assets: ['elasticsearch'],
+    operations: [
+        {
+            _op: 'elasticsearch_reader',
+            index: 'replace-me-100',
+            type: 'events',
+            size: 100,
+            date_field_name: 'created',
+            preserve_id: true
+        },
+        {
+            _op: 'elasticsearch_bulk',
+            type: 'change',
+            index: 'replace-me-1000',
+            preserve_id: true,
+            size: 50
+        }
+    ]
+};
---
+++ packages/teraslice/src/lib/storage/backends/mappings/state.ts
@@ -0,0 +1,37 @@
+export default {
+    template: '__state*',
+    mappings: {
+        state: {
+            _all: {
+                enabled: false
+            },
+            dynamic: false,
+            properties: {
+                ex_id: {
+                    type: 'keyword'
+                },
+                slice_id: {
+                    type: 'keyword'
+                },
+                slicer_id: {
+                    type: 'keyword'
+                },
+                slicer_order: {
+                    type: 'integer'
+                },
+                state: {
+                    type: 'keyword'
+                },
+                _created: {
+                    type: 'date'
+                },
+                _updated: {
+                    type: 'date'
+                },
+                error: {
+                    type: 'keyword'
+                }
+            }
+        }
+    }
+};
