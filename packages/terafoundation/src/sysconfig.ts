c4bc2bc133f4666c8ddf71935ad9d45359c64b36
Jared Noble <jsnoble1@gmail.com>
2024-06-07 15:01:59 -0700

    fix lint error

--- packages/terafoundation/src/sysconfig.ts
+++ packages/terafoundation/src/sysconfig.ts
@@ -3,7 +3,6 @@ import path from 'node:path';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import yaml from 'js-yaml';
-import { cloneDeep } from '@terascope/utils';
 import { ParsedArgs } from './interfaces.js';
 
 export function getDefaultConfigFile(): string|undefined {
