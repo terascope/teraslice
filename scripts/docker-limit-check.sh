15246f7cf80c0f2d5069ff0e709ca8118013fe18
busma13 <pluitjens@terascope.io>
2024-07-08 16:18:57 -0700

    fix script

--- scripts/docker-limit-check.sh
+++ scripts/docker-limit-check.sh
@@ -3,6 +3,6 @@
 ## This checks the docker rate limit
 ###
 TOKEN=$(curl --user "$USER:$PASS" "https://auth.docker.io/token?service=registry.docker.io&scope=repository:ratelimitpreview/test:pull" | jq -r .token)
-result=$(curl -s --head -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest)
+result=$(curl -sS --head -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest)
 
 echo "$result"
