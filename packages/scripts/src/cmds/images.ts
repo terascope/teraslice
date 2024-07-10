473ca3b7bda412f8fac28c1afb975010728da8e3
busma13 <pluitjens@terascope.io>
2024-07-10 11:40:22 -0700

    images load cmd

--- packages/scripts/src/cmds/images.ts
+++ packages/scripts/src/cmds/images.ts
@@ -13,8 +13,16 @@ const cmd: CommandModule<GlobalCMDOptions, Options> = {
     builder(yargs) {
         return yargs
             .example('$0 images list', 'Get the list of images needed for a test.')
-            .example('$0 images load', 'Load the docker images needed for a test.')
+            .example('$0 images load --directory \'./e2e\' --script \'yarn test:elasticsearch7\'', 'Load the docker images needed for a test.')
             .example('$0 images save', 'Save the docker images needed for a test.')
+            .option('directory', {
+                type: 'string',
+                description: 'Directory from which the test script we are loading images for will run.'
+            })
+            .option('script', {
+                type: 'string',
+                description: 'Script that test will use. This is needed to determine which images are needed.'
+            })
             .positional('action', {
                 description: 'The action to take',
                 choices: Object.values(ImagesAction),
@@ -22,11 +30,24 @@ const cmd: CommandModule<GlobalCMDOptions, Options> = {
                     return arg;
                 },
             })
+            .check((args) => {
+                if (args.action === ImagesAction.Load
+                    && !args.directory
+                    && !args.script
+                ) {
+                    throw new Error('"--script" and "--directory" options are required with "load" action');
+                }
+                return true;
+            })
             .requiresArg('action');
     },
     async handler(argv) {
         if (argv.action) {
-            await images(argv.action);
+            await images(
+                argv.action,
+                argv.directory as string,
+                argv.script as string
+            );
         }
     },
 };
--- packages/scripts/src/helpers/images/index.ts
+++ packages/scripts/src/helpers/images/index.ts
@@ -3,14 +3,18 @@ import execa from 'execa';
 import path from 'node:path';
 import * as config from '../config';
 import { ImagesAction } from './interfaces';
+import { getRootDir } from '../misc';
 
-export async function images(action: ImagesAction): Promise<void> {
+export async function images(action: ImagesAction,
+    directory: string,
+    script: string
+): Promise<void> {
     if (action === ImagesAction.List) {
         return createImageList('./images');
     }
 
     if (action === ImagesAction.Load) {
-        return loadImages('./images');
+        return loadImagesFromCache('/tmp/docker_cache/', directory, script);
     }
 
     if (action === ImagesAction.Save) {
@@ -26,25 +30,98 @@ async function createImageList(imagesPath: string): Promise<void> {
     const list = 'terascope/node-base:18.19.1\n'
                + 'terascope/node-base:20.11.1\n'
                + 'terascope/node-base:22.2.0\n'
-               + `${config.ELASTICSEARCH_DOCKER_IMAGE}:6.8.6\n`
+            //    + `${config.ELASTICSEARCH_DOCKER_IMAGE}:6.8.6\n`
                + `${config.ELASTICSEARCH_DOCKER_IMAGE}:7.9.3\n`
                + `${config.OPENSEARCH_DOCKER_IMAGE}:1.3.10\n`
                + `${config.OPENSEARCH_DOCKER_IMAGE}:2.8.0\n`
                + `${config.KAFKA_DOCKER_IMAGE}:7.1.9\n`
                + `${config.ZOOKEEPER_DOCKER_IMAGE}:7.1.9\n`
-               + `${config.MINIO_DOCKER_IMAGE}:RELEASE.2020-02-07T23-28-16Z`;
+               + `${config.MINIO_DOCKER_IMAGE}:RELEASE.2020-02-07T23-28-16Z\n`
+               + 'kindest/node:v1.30.0';
     if (!fse.existsSync(imagesPath)) {
         await fse.emptyDir(imagesPath);
     }
     fse.writeFileSync(path.join(imagesPath, 'image-list.txt'), list);
 }
 
-async function loadImages(imagesPath: string): Promise<void> {
-    try {
-        const imagesString = fse.readFileSync(path.join(imagesPath, 'image-list.txt'), 'utf-8');
-        const imagesArray = imagesString.split('\n');
+async function loadImagesFromCache(
+    cachePath: string,
+    directory: string,
+    script: string
+): Promise<void> {
+    console.log('@@@@ script: ', script);
+    console.log('@@@@ directory: ', directory);
 
-        const promiseArray = imagesArray.map(async (imageName) => execa.command(`docker load ${imageName}`));
+    const list = {
+        '18.19.1': 'terascope/node-base:18.19.1',
+        '20.11.1': 'terascope/node-base:20.11.1',
+        '22.2.0': 'terascope/node-base:22.2.0',
+        elasticsearch6: `${config.ELASTICSEARCH_DOCKER_IMAGE}:6.8.6`,
+        elasticsearch7: `${config.ELASTICSEARCH_DOCKER_IMAGE}:7.9.3`,
+        opensearch1: `${config.OPENSEARCH_DOCKER_IMAGE}:1.3.10`,
+        opensearch2: `${config.OPENSEARCH_DOCKER_IMAGE}:2.8.0`,
+        kafka: `${config.KAFKA_DOCKER_IMAGE}:7.1.9`,
+        zookeeper: `${config.ZOOKEEPER_DOCKER_IMAGE}:7.1.9`,
+        minio: `${config.MINIO_DOCKER_IMAGE}:RELEASE.2020-02-07T23-28-16Z`,
+        kind: 'kindest/node:v1.30.0'
+    };
+
+    const directoryPackageInfo = fse.readJSONSync(path.join(getRootDir(), `${directory}`, 'package.json'));
+    console.log('@@@@ directoryPackageInfo.scripts: ', directoryPackageInfo.scripts['test:elasticsearch6']);
+
+    try {
+        const imagesList: string[] = [];
+        script.split(' ').forEach((string, idx, arr) => {
+            if (string.startsWith('test:')) {
+                console.log('@@@@ string: ', string);
+                // find all images called for in the package.json
+                const scriptValue: string = directoryPackageInfo.scripts[string.trim()];
+                console.log('@@@@ scriptValue: ', scriptValue);
+
+                scriptValue.split(' ').forEach((part, scriptIdx, scriptArr) => {
+                    if (part === 'TEST_KAFKA=\'true\'') {
+                        imagesList.push(list.kafka);
+                        imagesList.push(list.zookeeper);
+                    }
+                    if (part === 'TEST_MINIO=\'true\'') {
+                        imagesList.push(list.minio);
+                    }
+                    if (part === 'TEST_OPENSEARCH=\'true\'') {
+                        if (scriptArr[scriptIdx + 1].startsWith('OPENSEARCH_VERSION=\'2')) {
+                            imagesList.push(list.opensearch2);
+                        } else {
+                            imagesList.push(list.opensearch1);
+                        }
+                    }
+                    if (part === 'TEST_ELASTICSEARCH=\'true\'') {
+                        if (scriptArr[scriptIdx + 1].startsWith('ELASTICSEARCH_VERSION=\'7')) {
+                            imagesList.push(list.elasticsearch7);
+                        } else {
+                            imagesList.push(list.elasticsearch6);
+                        }
+                    }
+                });
+            }
+            if (string === '--node-version') {
+                // load the base-docker-image with the called for node version
+                if (list[arr[idx + 1]]) {
+                    imagesList.push(list[arr[idx + 1]]);
+                }
+            }
+            return '';
+        });
+
+        console.log('@@@@ imageList: ', imagesList);
+
+        const promiseArray = imagesList.map(async (imageName) => {
+            const fileName = imageName.replace(/[/:]/g, '_');
+            const filePath = path.join(cachePath, `${fileName}.tar.gz`);
+            if (!fse.existsSync(filePath)) {
+                throw new Error(`No file found at ${filePath}. Have you restored the cache?`);
+            }
+            execa.command(`gunzip -c ${filePath} | docker load`);
+            fse.removeSync(filePath.slice(0, -3));
+        });
 
         await Promise.all(promiseArray);
     } catch (err) {
