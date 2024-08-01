62ffa27ff746a0697f9dd440bf21e5f711f5e885
Jared Noble <jsnoble1@gmail.com>
2024-08-01 15:03:30 -0700

    removed unneded statement

--- jest.config.base.js
+++ jest.config.base.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 
-export const isCI = process.env.CI === 'true';
+const isCI = process.env.CI === 'true';
 
 module.exports = (projectDir) => {
     let parentFolder;
