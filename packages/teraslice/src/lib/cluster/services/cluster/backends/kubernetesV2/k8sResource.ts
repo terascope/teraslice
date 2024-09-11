732dc3cc325cc26945907bdb2fc564c1cf3a2fb9
Joseph Soto <jsoto@terascope.com>
2024-09-11 13:40:37 -0700

    [teraslice] - Add execution controller restart logic in kubernetesV2 (#3740)
    This PR makes the following changes:
    
    ## New features
    - Teraslice running in `KubernetesV2` will have the ability to restart
    in a new pod automatically under certain conditions
    - These conditions include receiving a `SIGTERM` signal not associated
    with a job shutdown, and the slicer being restartable.
    - Added new function `isRestartable()` to the base slicer 
    - `isRestartable()` will return a `boolean` to tell wether the slicer is
    compatible with the restartable feature or not. This allows for an
    initial implementation for `kafka` slicers without having to worry about
    the complexity of other slicers.
    
    refs: #1007

--- packages/teraslice/src/lib/cluster/services/cluster/backends/kubernetesV2/k8sResource.ts
+++ packages/teraslice/src/lib/cluster/services/cluster/backends/kubernetesV2/k8sResource.ts
@@ -115,7 +115,6 @@ export class K8sResource {
     }
 
     _setEnvVariables() {
-        /// TODO: Use this later when we need to set env vars in workers/ex controllers
     }
 
     _mountLocalTeraslice(contextType: string): void {
@@ -292,14 +291,14 @@ export class K8sResource {
                 // eslint-disable-next-line max-len
                 this.resource.spec.template.spec.priorityClassName = this.terasliceConfig.kubernetes_priority_class_name;
                 if (this.execution.stateful) {
-                     
+
                     this.resource.spec.template.metadata.labels[`${this.jobPropertyLabelPrefix}/stateful`] = 'true';
                 }
             }
             if (this.nodeType === 'worker' && this.execution.stateful) {
                 // eslint-disable-next-line max-len
                 this.resource.spec.template.spec.priorityClassName = this.terasliceConfig.kubernetes_priority_class_name;
-                 
+
                 this.resource.spec.template.metadata.labels[`${this.jobPropertyLabelPrefix}/stateful`] = 'true';
             }
         }
