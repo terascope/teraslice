450271eca2b75b9da40fc444df8ff1238d0615fe
busma13 <pluitjens@terascope.io>
2024-09-20 08:20:39 -0700

    update nanoid from 3.3.4 to 5.0.7

--- packages/teraslice-messaging/src/utils/index.ts
+++ packages/teraslice-messaging/src/utils/index.ts
@@ -1,9 +1,12 @@
 import os from 'node:os';
 import url from 'node:url';
-import { nanoid } from 'nanoid/async';
+import { nanoid } from 'nanoid';
 
 export function newMsgId(): Promise<string> {
-    return nanoid(10);
+    return new Promise((resolve) => {
+        const id = nanoid(10);
+        resolve(id);
+    });
 }
 
 export function formatURL(hostname: string, port: number): string {
