2a9b5564b4558ac249391b7d97bd74d0c8721beb
sotojn <jsoto@terascope.com>
2024-09-20 13:43:22 -0700

    add check for relocating on ex pod init

--- packages/teraslice/src/lib/workers/execution-controller/index.ts
+++ packages/teraslice/src/lib/workers/execution-controller/index.ts
@@ -428,16 +428,24 @@ export class ExecutionController {
             this.context.sysconfig.teraslice.cluster_manager_type === 'kubernetesV2'
             && eventType === 'SIGTERM'
         ) {
-            await this.stateStorage.refresh();
-            const status = await this.executionStorage.getStatus(this.exId);
-            const runningStatuses = this.executionStorage.getRunningStatuses();
-            this.logger.debug(`Execution ${this.exId} is currently in a ${status} state`);
-            /// This is an indication that the cluster_master did not call for this
-            /// shutdown. We want to restart in this case.
-            if (status !== 'stopping' && includes(runningStatuses, status)) {
-                this.logger.info('Skipping shutdown to allow for relocation...');
-                return;
+            if (this.executionContext.slicer().isRelocatable) {
+                if (this.executionContext.slicer().isRelocatable()) {
+                    await this.stateStorage.refresh();
+                    const status = await this.executionStorage.getStatus(this.exId);
+                    const runningStatuses = this.executionStorage.getRunningStatuses();
+                    this.logger.debug(`Execution ${this.exId} is currently in a ${status} state`);
+                    /// This is an indication that the cluster_master did not call for this
+                    /// shutdown. We want to relocate in this case.
+                    if (status !== 'stopping' && includes(runningStatuses, status)) {
+                        this.logger.info('Setting  execution status to relocating');
+                        await this.executionStorage.setStatus(this.exId, 'relocating');
+                        this.logger.info('Skipping shutdown to allow for relocation...');
+                        return;
+                    }
+                }
             }
+            this.logger.warn('This slicer is not relocatable and will continue to shutdown.');
+
         }
 
         if (this.isShutdown) return;
@@ -941,12 +949,12 @@ export class ExecutionController {
         if (includes(terminalStatuses, status)) {
             error = new Error(invalidStateMsg('terminal'));
         } else if (includes(runningStatuses, status)) {
-            // In the case of a running status on startup we
+            // In the case of a relocating status on startup we
             // want to continue to start up. Only in V2.
-            // Right now we will depend on kubernetes `crashloopbackoff` in the case of
-            // an unexpected exit to the ex process. Ex: an OOM
-            // NOTE: If this becomes an issue we may want to add a new state. Maybe `interrupted`
-            if (this.context.sysconfig.teraslice.cluster_manager_type === 'kubernetesV2') {
+            if (
+                this.context.sysconfig.teraslice.cluster_manager_type === 'kubernetesV2'
+                && status === 'relocating'
+            ) {
                 // Check to see if `isRelocatable` exists.
                 // Allows for older assets to work with k8sV2
                 if (this.executionContext.slicer().isRelocatable) {
@@ -959,6 +967,7 @@ export class ExecutionController {
                     }
                     return relocatable;
                 }
+                this.logger.warn('The slicer cannot relocate because it is not relocatable. Shutting down.');
             }
             error = new Error(invalidStateMsg('running'));
             // If in a running status the execution process
