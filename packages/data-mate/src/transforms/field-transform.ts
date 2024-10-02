b6989b03a5782555658c614bff2598534cbd2541
busma13 <pluitjens@terascope.io>
2024-10-02 09:56:08 -0700

    ternary not needed. pull logic out of fn parameter

--- packages/data-mate/src/transforms/field-transform.ts
+++ packages/data-mate/src/transforms/field-transform.ts
@@ -1379,7 +1379,9 @@ function _parseDate(input: any, args: ParseDateConfig) {
         throw new Error(`Invalid parameter format, must be a string, received ${ts.getTypeOf(input)}`);
     }
 
-    const parsed = parse(ts.isNumber(input) ? input.toString() : input, format, new Date());
+    const inputStr = input.toString();
+
+    const parsed = parse(inputStr, format, new Date());
 
     if (!ts.isValidDateInstance(parsed)) {
         throw new Error('Cannot parse date');
