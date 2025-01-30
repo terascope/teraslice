43f1ee564ce650af9836d999c83a430c7ac55601
busma13 <pluitjens@terascope.io>
2025-01-30 15:02:20 -0700

    update opensearch test versions

--- packages/scripts/src/helpers/config.ts
+++ packages/scripts/src/helpers/config.ts
@@ -19,9 +19,9 @@ export const __DEFAULT_ELASTICSEARCH6_VERSION = '6.8.6';
 /** Default elasticsearch7 version used to populate the CI cache */
 export const __DEFAULT_ELASTICSEARCH7_VERSION = '7.9.3';
 /** Default opensearch1 version used to populate the CI cache */
-export const __DEFAULT_OPENSEARCH1_VERSION = '1.3.10';
+export const __DEFAULT_OPENSEARCH1_VERSION = '1.3.11';
 /** Default opensearch2 version used to populate the CI cache */
-export const __DEFAULT_OPENSEARCH2_VERSION = '2.8.0';
+export const __DEFAULT_OPENSEARCH2_VERSION = '2.15.0';
 
 export const TERASLICE_PORT = '45678';
 export const HOST_IP = process.env.HOST_IP || address();
