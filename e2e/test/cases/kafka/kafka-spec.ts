c671139f9431a4668c58df295edd2805c7322190
busma13 <pluitjens@terascope.io>
2025-02-13 15:53:07 -0700

    add encrypted kafka tests

--- e2e/test/cases/kafka/kafka-spec.ts
+++ e2e/test/cases/kafka/kafka-spec.ts
@@ -1,7 +1,8 @@
 import { v4 as uuidv4 } from 'uuid';
 import { TerasliceHarness } from '../../teraslice-harness.js';
 import signale from '../../signale.js';
-import { TEST_PLATFORM } from '../../config.js';
+import { ENCRYPT_KAFKA, ROOT_CERT_PATH, TEST_PLATFORM } from '../../config.js';
+import { exec } from '@terascope/scripts';
 
 describe('kafka', () => {
     let terasliceHarness: TerasliceHarness;
@@ -64,5 +65,18 @@ describe('kafka', () => {
         }
 
         expect(count).toBe(total);
+    });
+
+    describe('encrypted kafka', () => {
+        if (ENCRYPT_KAFKA === 'true') {
+            it('should have an encrypted connection', async () => {
+                const result = await exec({
+                    cmd: 'sh',
+                    args: ['-c', `printf '\\n' | openssl s_client -connect localhost:49092 -CAfile ${ROOT_CERT_PATH}`]
+                });
+                // console.log('@@@@@@ s_client output: ', result);
+                expect(result).toContain('Verification: OK');
+            });
+        }
     });
 });
