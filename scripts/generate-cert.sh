7410550a4bb8d133231acde75c6a3d0335ce0ac4
sotojn <jsoto@terascope.com>
2025-02-27 16:56:30 -0700

    add create_internal_users_file func to cert gen script

--- scripts/generate-cert.sh
+++ scripts/generate-cert.sh
@@ -31,6 +31,8 @@ format() {
     mv $PRIVATE_KEY_NAME private.key
     local PUBLIC_CERT_NAME=$(ls | grep -i ".pem")
     mv $PUBLIC_CERT_NAME public.crt
+    cp private.key opensearch-key.pem
+    cp public.crt opensearch-cert.pem
 }
 
 grab_rootCA() {
@@ -60,14 +62,78 @@ generate() {
 
     mkdir "$CERT_DIR"
     cd "$CERT_DIR"
-    mkcert $DNS_NAMES
+    mkcert --client $DNS_NAMES
 
     grab_rootCA "$CERT_DIR"
     format "$CERT_DIR"
+    create_internal_users_file "$CERT_DIR"
 
     echo "Successfully created certificates at $CERT_DIR"
     exit 0
 
+}
+
+create_internal_users_file() {
+    local CERT_DIR="$1"
+
+    cat <<EOF > "$CERT_DIR/internal_users.yml"
+# This is the internal user database
+# The hash value is a bcrypt hash and can be generated with plugin/tools/hash.sh
+
+_meta:
+  type: "internalusers"
+  config_version: 2
+
+# Define your internal users here
+
+## Demo users
+# This hash password is passwordsufhbivbU123%$
+admin:
+  hash: "\$2y\$12\$Z234bambHnVJMAXiccuMluNgGhNNdOFIY6pFT2/lk3ZC.RDoDIFme"
+  reserved: true
+  backend_roles:
+  - "admin"
+  description: "Demo admin user"
+anomalyadmin:
+  hash: "\$2y\$12\$TRwAAJgnNo67w3rVUz4FIeLx9Dy/llB79zf9I15CKJ9vkM4ZzAd3."
+  reserved: false
+  opendistro_security_roles:
+  - "anomaly_full_access"
+  description: "Demo anomaly admin user, using internal role"
+kibanaserver:
+  hash: "\$2a\$12\$4AcgAt3xwOWadA5s5blL6ev39OXDNhmOesEoo33eZtrq2N0YrU3H."
+  reserved: true
+  description: "Demo OpenSearch Dashboards user"
+kibanaro:
+  hash: "\$2a\$12\$JJSXNfTowz7Uu5ttXfeYpeYE0arACvcwlPBStB1F.MI7f0U9Z4DGC"
+  reserved: false
+  backend_roles:
+  - "kibanauser"
+  - "readall"
+  attributes:
+    attribute1: "value1"
+    attribute2: "value2"
+    attribute3: "value3"
+  description: "Demo OpenSearch Dashboards read only user, using external role mapping"
+logstash:
+  hash: "\$2a\$12\$u1ShR4l4uBS3Uv59Pa2y5.1uQuZBrZtmNfqB3iM/.jL0XoV9sghS2"
+  reserved: false
+  backend_roles:
+  - "logstash"
+  description: "Demo logstash user, using external role mapping"
+readall:
+  hash: "\$2a\$12\$ae4ycwzwvLtZxwZ82RmiEunBbIPiAmGZduBAjKN0TXdwQFtCwARz2"
+  reserved: false
+  backend_roles:
+  - "readall"
+  description: "Demo readall user, using external role mapping"
+snapshotrestore:
+  hash: "\$2y\$12\$DpwmetHKwgYnorbgdvORCenv4NAK8cPUg8AI6pxLCuWf/ALc0.v7W"
+  reserved: false
+  backend_roles:
+  - "snapshotrestore"
+  description: "Demo snapshotrestore user, using external role mapping"
+EOF
 }
 
 
