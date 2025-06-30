42e74b434f0e389a309a5cd576baf72f8374c79e
Lesley Dreyer <lesleydreyer@hotmail.com>
2025-06-30 16:08:07 -0700

    table tests

--- packages/xlucene-translator/test/cases/queries/index.ts
+++
@@ -1,9 +0,0 @@
-import type { TestCaseCollection } from './interfaces.js';
-import { arrays, empty_arrays } from './arrays.js';
-
-const tests: TestCaseCollection = {
-    arrays: arrays,
-    empty_arrays,
-};
-
-export default tests;
