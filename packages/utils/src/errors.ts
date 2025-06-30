ee1dfb81e263adcc0ca39ac3c85f35792628e632
Joseph Soto <jsoto@terascope.com>
2025-06-30 14:47:17 -0700

    Update packages/utils/src/errors.ts
    Co-authored-by: Austin Godber <godber@terascope.io>

--- packages/utils/src/errors.ts
+++ packages/utils/src/errors.ts
@@ -480,7 +480,7 @@ export function stripErrorMessage(
 }
 
 /**
- * Formats an AggregateError into a user-friendly Error.
+ * Formats an AggregateError into a user-friendly Error which shows the first five Errors from the Aggregate.
  * @param aggregateError
  */
 export async function formatAggregateError(aggregateError: unknown) {
