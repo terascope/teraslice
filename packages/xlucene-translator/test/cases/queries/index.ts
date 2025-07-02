b83b4603cc385d3a7245d19f726423d4bf2394ca
Lesley Dreyer <lesleydreyer@hotmail.com>
2025-07-01 18:17:35 -0700

    [skip ci] add search tests

---
+++ packages/xlucene-translator/test/cases/queries/index.ts
@@ -0,0 +1,9 @@
+import type { TestCaseCollection } from './interfaces.js';
+import { arrays, empty_arrays } from './arrays.js';
+
+const tests: TestCaseCollection = {
+    arrays: arrays,
+    empty_arrays,
+};
+
+export default tests;
