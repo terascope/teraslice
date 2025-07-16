4fa77510c12253b4ad73b02ad0a2c17db4820fb6
Lesley Dreyer <lesleydreyer@hotmail.com>
2025-07-15 18:06:16 -0700

    [skip ci]  reverts

--- packages/test-utils/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './elasticsearch/config.js';
-export * from './elasticsearch/elasticsearch.js';
-export * from './data/fixed-data/index.js';
-export * from './data/data-type-generator.js';
