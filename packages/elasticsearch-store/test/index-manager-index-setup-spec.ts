b2bd025aae88e9acfb601cd4d2f857d1cc5d32e9
Jared Noble <jsnoble1@gmail.com>
2025-10-07 09:37:33 -0700

    fix teraslice mappings to remove types

--- packages/elasticsearch-store/test/index-manager-index-setup-spec.ts
+++ packages/elasticsearch-store/test/index-manager-index-setup-spec.ts
@@ -296,7 +296,6 @@ describe('IndexManager->indexSetup()', () => {
             const { version: schemaVersion } = config.index_schema!;
 
             const { mappings } = config.data_type.toESMapping({
-                typeName: config.name,
                 ...clientMetadata
             });
 
