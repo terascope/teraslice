c0764400c1d4f510c37ce4d9263791e9e29d2127
sotojn <jsoto@terascope.com>
2025-10-28 09:46:40 -0700

    add ssl config to kafka docker service

--- e2e/test/setup-config.ts
+++ e2e/test/setup-config.ts
@@ -6,7 +6,8 @@ import {
     TEST_HOST, TERASLICE_PORT, CLUSTER_NAME,
     HOST_IP, CONFIG_PATH, ASSET_STORAGE_CONNECTION,
     ASSET_STORAGE_CONNECTION_TYPE, MINIO_HOST,
-    ENCRYPT_MINIO, ROOT_CERT_PATH, FILE_LOGGING
+    ENCRYPT_MINIO, ROOT_CERT_PATH, FILE_LOGGING,
+    ENCRYPT_KAFKA
 } from './config.js';
 
 const baseConfig = {
@@ -31,7 +32,9 @@ const baseConfig = {
             },
             kafka: {
                 default: {
-                    brokers: [KAFKA_BROKER]
+                    brokers: [KAFKA_BROKER],
+                    security_protocol: '',
+                    caCertificate: ''
                 }
             },
             s3: {
@@ -106,6 +109,12 @@ async function writeMasterConfig() {
         masterConfig.terafoundation.connectors.s3.default.caCertificate = rootCA;
     }
 
+    if (ENCRYPT_KAFKA === 'true') {
+        const rootCA = fse.readFileSync(ROOT_CERT_PATH, 'utf8');
+        masterConfig.terafoundation.connectors.kafka.default.security_protocol = 'ssl';
+        masterConfig.terafoundation.connectors.kafka.default.caCertificate = rootCA;
+    }
+
     const masterConfigPath = path.join(CONFIG_PATH, 'teraslice-master.json');
     await fse.writeJSON(masterConfigPath, masterConfig, {
         spaces: 4
@@ -119,6 +128,12 @@ async function writeWorkerConfig() {
         const rootCA = fse.readFileSync(ROOT_CERT_PATH, 'utf8');
         workerConfig.terafoundation.connectors.s3.default.sslEnabled = true;
         workerConfig.terafoundation.connectors.s3.default.caCertificate = rootCA;
+    }
+
+    if (ENCRYPT_KAFKA === 'true') {
+        const rootCA = fse.readFileSync(ROOT_CERT_PATH, 'utf8');
+        workerConfig.terafoundation.connectors.kafka.default.security_protocol = 'ssl';
+        workerConfig.terafoundation.connectors.kafka.default.caCertificate = rootCA;
     }
 
     const workerConfigPath = path.join(CONFIG_PATH, 'teraslice-worker.json');
