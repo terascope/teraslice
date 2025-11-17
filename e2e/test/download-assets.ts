ce8ff6657042d3f267f9076dc52eb127d516dd8a
busma13 <pluitjens@terascope.io>
2025-11-17 11:49:22 -0700

    Allow e2e constants to be used in downloadAssets.js script without env vars present

--- e2e/test/download-assets.ts
+++ e2e/test/download-assets.ts
@@ -5,8 +5,7 @@ import semver from 'semver';
 import { downloadRelease, HTTPError } from '@terascope/fetch-github-release';
 import { pRetry, isCI } from '@terascope/core-utils';
 import signale from './signale.js';
-import { config } from './config.js';
-const { AUTOLOAD_PATH, ASSET_BUNDLES_PATH, USE_DEV_ASSETS } = config;
+import { AUTOLOAD_PATH, ASSET_BUNDLES_PATH, USE_DEV_ASSETS } from './constants.js';
 
 type AssetInfo = {
     name: string;
