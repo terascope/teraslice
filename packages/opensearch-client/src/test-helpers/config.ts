b1ff94d6133a437b125dcd305273e2bd47ad0428
busma13 <pluitjens@terascope.io>
2025-11-18 07:49:03 -0700

    temp fix for OS client test helpers

--- packages/opensearch-client/src/test-helpers/config.ts
+++ packages/opensearch-client/src/test-helpers/config.ts
@@ -1,14 +1,14 @@
 import { z } from 'zod';
 
 const opensearchEnvSchema = z.object({
-    ELASTICSEARCH_HOST: z.string(),
-    ELASTICSEARCH_VERSION: z.string(),
-    OPENSEARCH_HOST: z.string(),
-    OPENSEARCH_PASSWORD: z.string(),
-    OPENSEARCH_SSL_HOST: z.string(),
-    OPENSEARCH_USER: z.string(),
-    OPENSEARCH_VERSION: z.string(),
-    RESTRAINED_OPENSEARCH_HOST: z.string(),
+    ELASTICSEARCH_HOST: z.string().optional(),
+    ELASTICSEARCH_VERSION: z.string().optional(),
+    OPENSEARCH_HOST: z.string().optional(),
+    OPENSEARCH_PASSWORD: z.string().optional(),
+    OPENSEARCH_SSL_HOST: z.string().optional(),
+    OPENSEARCH_USER: z.string().optional(),
+    OPENSEARCH_VERSION: z.string().optional(),
+    RESTRAINED_OPENSEARCH_HOST: z.string().optional(),
     SEARCH_TEST_HOST: z.string(),
     TEST_INDEX_PREFIX: z.string(),
 });
