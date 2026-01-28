cb4cb05c0181046655290ece598340825f8e13ac
sotojn <jsoto@terascope.com>
2026-01-28 08:13:35 -0700

    add more fields to job and ex mappings

--- packages/teraslice/src/lib/storage/backends/mappings/job.ts
+++ packages/teraslice/src/lib/storage/backends/mappings/job.ts
@@ -3,12 +3,41 @@ import { DataTypeConfig, FieldType } from '@terascope/types';
 export const jobDataTypeConfig: DataTypeConfig = {
     version: 1,
     fields: {
-        active: { type: FieldType.Boolean },
+        // JobConfig fields
         job_id: { type: FieldType.Keyword },
         _context: { type: FieldType.Keyword },
         _created: { type: FieldType.Date },
         _updated: { type: FieldType.Date },
         _deleted: { type: FieldType.Boolean },
-        _deleted_on: { type: FieldType.Date }
+        _deleted_on: { type: FieldType.Date },
+        // ValidatedJobConfig fields
+        active: { type: FieldType.Boolean },
+        analytics: { type: FieldType.Boolean },
+        assets: { type: FieldType.Keyword },
+        assetIds: { type: FieldType.Keyword },
+        autorecover: { type: FieldType.Boolean },
+        lifecycle: { type: FieldType.Keyword },
+        max_retries: { type: FieldType.Integer },
+        name: { type: FieldType.Text },
+        probation_window: { type: FieldType.Integer },
+        performance_metrics: { type: FieldType.Boolean },
+        log_level: { type: FieldType.Keyword },
+        slicers: { type: FieldType.Integer },
+        workers: { type: FieldType.Integer },
+        stateful: { type: FieldType.Boolean },
+        // K8s fields
+        cpu: { type: FieldType.Float },
+        cpu_execution_controller: { type: FieldType.Float },
+        ephemeral_storage: { type: FieldType.Boolean },
+        memory: { type: FieldType.Integer },
+        memory_execution_controller: { type: FieldType.Integer },
+        resources_requests_cpu: { type: FieldType.Float },
+        resources_requests_memory: { type: FieldType.Integer },
+        resources_limits_cpu: { type: FieldType.Float },
+        resources_limits_memory: { type: FieldType.Integer },
+        kubernetes_image: { type: FieldType.Text },
+        prom_metrics_enabled: { type: FieldType.Boolean },
+        prom_metrics_port: { type: FieldType.Integer },
+        prom_metrics_add_default: { type: FieldType.Boolean }
     }
 };
