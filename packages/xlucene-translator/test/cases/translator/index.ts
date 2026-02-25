24658e341cba022f2b6bc5fd77d8a60484f783db
Jared Noble <jsnoble1@gmail.com>
2026-02-25 08:34:28 -0700

    removed uneeded file since we test against real opensearch

--- packages/xlucene-translator/test/cases/translator/index.ts
+++ packages/xlucene-translator/test/cases/translator/index.ts
@@ -2,16 +2,14 @@ import fieldGroup from './field-group.js';
 import geo from './geo.js';
 import logicalGroup from './logical-group.js';
 import termLevel from './term.js';
-import ipRange from './ip-range.js';
 import arrays from './arrays.js';
 
 export * from './interfaces.js';
 
 export default {
-    // term_level: termLevel,
-    // geo,
-    // field_group: fieldGroup,
-    // logical_group: logicalGroup,
-    ip_range: ipRange,
-    // arrays
+    term_level: termLevel,
+    geo,
+    field_group: fieldGroup,
+    logical_group: logicalGroup,
+    arrays
 };
