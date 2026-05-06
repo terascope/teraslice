628f23cafc7eed177673ebdd344e21eb3b5518be
Lesley Dreyer <lesleydreyer@hotmail.com>
2026-05-05 19:48:13 -0700

    fix prev

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -1,10 +1,10 @@
 import { fileURLToPath } from 'node:url';
-import baseConfig from '../jest.make-config.js';
+import makeConfig from '../jest.pkgs.make-config.js';
 import { URL } from 'node:url';
 
 const dirPath = fileURLToPath(new URL('.', import.meta.url));
 
-const config = baseConfig([dirPath]);
+const config = makeConfig(dirPath);
 
 config.collectCoverage = false;
 config.testTimeout = 3 * 60 * 1000;
--- packages/eslint-config/jest.config.js
+++ packages/eslint-config/jest.config.js
@@ -1,9 +1,9 @@
 import { fileURLToPath } from 'node:url';
-import makeConfig from '../../jest.make-config.js';
+import makeConfig from '../../jest.pkgs.make-config.js';
 
 const dirPath = fileURLToPath(new URL('.', import.meta.url));
 
-const config = makeConfig([dirPath]);
+const config = makeConfig(dirPath);
 
 config.globals.window = {};
 
