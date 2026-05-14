a222b0067391cf4a5a3cfc23fa95d4864e6b7e78
Lesley Dreyer <lesleydreyer@hotmail.com>
2026-05-14 11:38:57 -0700

    add comment

--- packages/teraslice-client-js/jest.config.js
+++ packages/teraslice-client-js/jest.config.js
@@ -4,7 +4,9 @@ import baseConfig from '../../jest.make-root-config.js';
 const dirPath = fileURLToPath(new URL('.', import.meta.url));
 
 // NOTE: flaky, fails a lot running in batch w/other packages in CI,
-// moved here to run w/less packages even though it's hacky
+// adding this config here is a hack to make it run w/less packages -
+// should investigate further if still an issue after we decide if
+// we're switching test frameworks
 const config = baseConfig(dirPath, false);
 
 export default config;
