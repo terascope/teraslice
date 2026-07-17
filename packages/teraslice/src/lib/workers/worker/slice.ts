980571c7495564f66a49c27fd0b6187cd41d98a1
Peter Luitjens <43619525+busma13@users.noreply.github.com>
2026-07-16 17:28:03 -0700

    [teraslice] add error log when race condition in slice_id identified (#4502)
    This PR adds an error log when slice_ids do not match during worker
    shutdown flush().
    
    Ref: #4495

--- packages/teraslice/src/lib/workers/worker/slice.ts
+++ packages/teraslice/src/lib/workers/worker/slice.ts
@@ -147,6 +147,10 @@ export class SliceExecution {
         await this.stateStorage.updateState(slice, SliceState.completed, undefined, retryCount);
 
         this.logger.trace(`completed slice for execution: ${this.executionContext.exId}`, slice);
+        if (this.logger.fields.slice_id !== slice.slice_id) {
+            // This log is used to identify this issue: https://github.com/terascope/teraslice/issues/4495
+            this.logger.error(`The slice_id of the slice: ${slice.slice_id} does not match the slice_id of this logger: ${this.logger.fields.slice_id}`);
+        }
         this.events.emit('slice:success', slice);
     }
 
