71488099e6915c3309490104c188b6413b7dc7f0
busma13 <pluitjens@terascope.io>
2026-07-27 13:38:33 -0700

    remove worker_version config fields, default to v2 worker code

--- packages/types/src/teraslice.ts
+++ packages/types/src/teraslice.ts
@@ -326,7 +326,6 @@ export interface ValidatedJobConfig {
     log_level?: string;
     slicers: number;
     workers: number;
-    worker_version?: WorkerVersion;
     stateful?: boolean;
     /** This will only be available in the context of k8s */
     labels?: { [key: string]: string };
@@ -573,7 +572,6 @@ export interface Config {
     state: { connection: string };
     env_vars: { [key: string]: string };
     worker_disconnect_timeout: number;
-    worker_version: WorkerVersion;
     workers: number;
 }
 
@@ -653,5 +651,3 @@ export type ChangeWorkerQueryParams = 'add' | 'remove' | 'total';
 export interface ChangeWorkerResponse {
     message: string;
 }
-
-export type WorkerVersion = 'v1' | 'v2';
